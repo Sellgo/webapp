6277746c089f6088ff84b1e8078830018f0b4d8e
dharmesh03 <dharmeshdev01@gmail.com>
2019-06-19 10:00:59 +0530

    Fix TSList and prettier issue

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,57 +1,68 @@
-import * as React from "react";
-import {Form, Segment, Button, Grid, GridRow} from "semantic-ui-react";
-import {Link} from "react-router-dom";
-import "./login.css";
-import {Logo} from "../../components/AdminLayout/AdminHeader";
-import IntroSlider from "../../components/IntroSlider";
-import PasswordShowHide from "../../components/Password/PasswordShowHide";
-import buttonStyle from "../../components/StyleComponent/StyleComponent";
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Form, Grid, GridRow, Segment } from 'semantic-ui-react'
+import { Logo } from '../../components/AdminLayout/AdminHeader'
+import IntroSlider from '../../components/IntroSlider'
+import PasswordShowHide from '../../components/Password/PasswordShowHide'
+import buttonStyle from '../../components/StyleComponent/StyleComponent'
+import './login.css'
 export class Login extends React.Component {
-
-  render() {
-
+  public render() {
     return (
-      <Grid verticalAlign='middle' style={{minHeight: "100vh"}}>
+      <Grid verticalAlign="middle" style={{ minHeight: '100vh' }}>
         <Grid.Row>
           <Grid.Column className="left-pane" width={10}>
-            <IntroSlider/>
+            <IntroSlider />
           </Grid.Column>
           <Grid.Column className="right-pane" width={6}>
             <div className="logo-img">
-              <Logo centered size="small"/>
+              <Logo centered={true} size="small" />
             </div>
-            <Segment basic clearing>
+            <Segment basic={true} clearing={true}>
               <Grid>
                 <Grid.Row>
                   <Grid.Column width={16}>
                     <Form className="p-t-40">
                       <Form.Field>
                         <div className="small-light login-fields">
-                          <input type="email" placeholder="Email Address" className="login-field1"/>
-                          <div className="hr_line"></div>
-                          <PasswordShowHide/>
+                          <input
+                            type="email"
+                            placeholder="Email Address"
+                            className="login-field1"
+                          />
+                          <div className="hr_line"/>
+                          <PasswordShowHide />
                         </div>
                       </Form.Field>
                     </Form>
                   </Grid.Column>
                   <Grid.Column width={16}>
-                        <Grid.Row width={16}>
-                          <Grid.Column className="small-regular textAlignCenter padding20">
-                            <Link to="/forgot-password" style={{fontSize: "smaller",color:"gray"}}>Forgot your password?</Link>   
-                          </Grid.Column>
-                        </Grid.Row>
-                        <GridRow>
-                          <div className="textAlignCenter">                    
-                            <Button as={Link} style={buttonStyle}
-                              className="primary-button" to='/dashboard'content="Sign In"/>                 
-                          </div>
-                        </GridRow>
-                        <GridRow >
-                        <div className="textAlignCenter padding20 p-t-40">
-                           <Link to="/sign-up" className="small-bold">Create My Sellgo Account!</Link>
-                        </div>
-                      </GridRow>
-                    </Grid.Column>
+                    <Grid.Row width={16}>
+                      <Grid.Column className="small-regular textAlignCenter padding20">
+                        <Link to="/forgot-password" style={{ fontSize: 'smaller', color: 'gray' }}>
+                          Forgot your password?
+                        </Link>
+                      </Grid.Column>
+                    </Grid.Row>
+                    <GridRow>
+                      <div className="textAlignCenter">
+                        <Button
+                          as={Link}
+                          style={buttonStyle}
+                          className="primary-button"
+                          to="/dashboard"
+                          content="Sign In"
+                        />
+                      </div>
+                    </GridRow>
+                    <GridRow>
+                      <div className="textAlignCenter padding20 p-t-40">
+                        <Link to="/sign-up" className="small-bold">
+                          Create My Sellgo Account!
+                        </Link>
+                      </div>
+                    </GridRow>
+                  </Grid.Column>
                 </Grid.Row>
               </Grid>
             </Segment>
@@ -61,4 +72,3 @@ export class Login extends React.Component {
     )
   }
 }
-
