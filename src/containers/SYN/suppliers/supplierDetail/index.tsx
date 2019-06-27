88f1722944e9debdeb3b42f7e2bf5be09eeb68d6
Egenie Next <egenie.apple@gmail.com>
2019-06-27 20:38:06 +0500

    products view in progress

--- src/containers/SYN/suppliers/supplierDetail/index.tsx
+++ src/containers/SYN/suppliers/supplierDetail/index.tsx
@@ -18,16 +18,15 @@ import {
   Popup,
   Modal,
   TextArea,
+  Card,
+  Feed,
 } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-// import MesssageComponent from '../../../components/MessageComponent';
-// import { Modals } from '../../../components/Modals';
-// import buttonStyle from '../../../components/StyleComponent/StyleComponent';
 import '../suppliers.css';
 
 import { Link } from 'react-router-dom';
 
-import { getProducts, getSellers, Product } from '../../../../Action/SYNActions';
+import { getProducts, getSellers, Product, Supplier } from '../../../../Action/SYNActions';
 
 interface State {
   isOpen: boolean;
@@ -41,7 +40,26 @@ interface Props {
 export class SupplierDetail extends React.Component<Props, State> {
   state = {
     isOpen: false,
-    size: '',
+    Products: [
+      {
+        productTitle: 'Test',
+        listCat: 'List Categories',
+        Profit: '10usd',
+        Margin: '10usd',
+        Sales_mo: '120',
+        profit_mo: '120USD',
+        lastSyn: '20/05/2019',
+      },
+      {
+        productTitle: 'Test',
+        listCat: 'List Categories',
+        Profit: '5usd',
+        Margin: '10usd',
+        Sales_mo: '9000',
+        profit_mo: '2000USD',
+        lastSyn: '15/06/2019',
+      },
+    ],
   };
   message = {
     id: 1,
@@ -67,28 +85,95 @@ export class SupplierDetail extends React.Component<Props, State> {
     });
   };
   renderTable = () => {
+    const productsTable = this.state.Products;
     return (
       <Table basic='very'>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>
+            <Table.HeaderCell width={1}>
               <Checkbox/>
             </Table.HeaderCell>
-            <Table.HeaderCell>
-              Supplier Name
+            <Table.HeaderCell width={4}>
+              Product Info
             </Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-            <Table.HeaderCell>Action</Table.HeaderCell>
-            <Table.HeaderCell>Product to Listing Ratio</Table.HeaderCell>
-            <Table.HeaderCell>Supplier Rate (%)</Table.HeaderCell>
-            <Table.HeaderCell>Note</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
+            <Table.HeaderCell width={1}/>
+            <Table.HeaderCell width={1}>Profit</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Margin</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Sales/mo</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Profit/Mo</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Add to Tracker</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Last Syn</Table.HeaderCell>
+            <Table.HeaderCell width={1}/>
           </Table.Row>
         </Table.Header>
+        <Table.Body>
+          {productsTable.map((value, index) => {
+            return (
+              <Table.Row key={index}>
+                <Table.Cell>
+                  <Checkbox/>
+                </Table.Cell>
+                <Table.Cell>
+                  <Grid>
+                    <Grid.Column width={2} floated="left">
+                      <Image src={new URL('http://localhost:3000/images/intro.png')} size='tiny'/>
+                    </Grid.Column>
+                    <Grid.Column width={8} floated="left" className={'middle aligned'}>
+                      <Grid.Row as={Link} to={`/syn/`}>
+                        {value.productTitle}
+                      </Grid.Row>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Image
+                            src={new URL('http://localhost:3000/images/intro.png')}
+                            size="mini"
+                          />
+                        </Grid.Column>
+                        <Grid.Column as={Link} to={`/syn/`}>
+                          {value.listCat}
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid.Column>
+                  </Grid>
+                </Table.Cell>
+                <Table.Cell>
+                  <Button basic style={{ borderRadius: 20 }} color='blue'>
+                    View
+                  </Button>
+                </Table.Cell>
+                <Table.Cell>
+                  {value.Profit}
+                </Table.Cell>
+                <Table.Cell>
+                  {value.Margin}
+                </Table.Cell>
+                <Table.Cell>
+                  {value.Sales_mo}
+                </Table.Cell>
+                <Table.Cell>
+                  {value.profit_mo}
+                </Table.Cell>
+                <Table.Cell>
+                  <Button basic style={{ borderRadius: 20 }} color='blue'>
+                    Track Now
+                  </Button>
+                </Table.Cell>
+                <Table.Cell>
+                  {value.lastSyn}
+                </Table.Cell>
+                <Table.Cell>
+                  <Table.Cell as={Link} to={`/syn/`}>
+                    <Icon name='amazon' style={{ color: 'black' }}/>&nbsp;
+                  </Table.Cell>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
         <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan='3'>
-              <Menu floated='right' pagination>
+          <Table.Row textAlign='center'>
+            <Table.HeaderCell colSpan={10}>
+              <Menu pagination>
                 <Menu.Item as='a' icon>
                   <Icon name='chevron left'/>
                 </Menu.Item>
@@ -107,6 +192,7 @@ export class SupplierDetail extends React.Component<Props, State> {
     );
   };
   renderDeleteModal = (value: Product, index: any) => {
+
     return (
       <Modal
         trigger={<Icon name="trash alternate" style={{ color: 'black' }}/>}
@@ -124,6 +210,90 @@ export class SupplierDetail extends React.Component<Props, State> {
       </Modal>
     );
   };
+  renderHeader = () => {
+    const items = [
+      {
+        header: 'Project Report - April',
+        description: 'Leverage agile',
+        meta: 'ROI: 30%',
+      },
+      {
+        header: 'Project Report - May',
+        description: 'Bring to the table',
+        meta: 'ROI: 34%',
+      },
+      {
+        header: 'Project Report - June',
+        description:
+          'Capitalise on low hanging fruit ',
+        meta: 'ROI: 27%',
+      },
+    ]
+    return (
+      <Grid>
+        <Grid.Column width={5} floated="left" className={'middle aligned'}>
+          <Grid.Row>
+            Syn Preset
+          </Grid.Row>
+          <Card>
+            <Card.Content>
+              <Card.Header>Recent Activity</Card.Header>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                <Feed.Event>
+                  <Feed.Label image='/images/avatar/small/jenny.jpg' />
+                  <Feed.Content>
+                    <Feed.Date content='1 day ago' />
+                    <Feed.Summary>
+                      You added <a>Jenny Hess</a> to your <a>coworker</a> group.
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Label image='/images/avatar/small/molly.png' />
+                  <Feed.Content>
+                    <Feed.Date content='3 days ago' />
+                    <Feed.Summary>
+                      You added <a>Molly Malone</a> as a friend.
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+
+                <Feed.Event>
+                  <Feed.Label image='/images/avatar/small/elliot.jpg' />
+                  <Feed.Content>
+                    <Feed.Date content='4 days ago' />
+                    <Feed.Summary>
+                      You added <a>Elliot Baker</a> to your <a>musicians</a> group.
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column width={5} >
+          <Card.Group items={items}>
+          </Card.Group>
+          <div
+            className="ui"
+            style={{
+              display: 'inline-flex',
+              border: '1px solid #000',
+              padding: '11px',
+              borderRadius: '15px',
+            }}
+          >
+          <span style={{ padding: '8px' }}>
+               MORE DETAILS
+              </span>
+          </div>
+        </Grid.Column>
+      </Grid>
+    );
+  };
 
   render() {
     const memberDate = `May 5 2018`;
@@ -131,31 +301,8 @@ export class SupplierDetail extends React.Component<Props, State> {
     return (
       <Segment basic={true} className="setting">
         <Divider/>
-        <Grid>
-          <Grid.Column width={5} floated="left" className={'middle aligned'}>
-            <Popup
-              trigger={<Icon name='question circle' circular/>}
-              content="Sellgo"
-              position="top left"
-              size="tiny"
-            />
-          </Grid.Column>
-          <Grid.Column width={5} floated="right">
-            <div
-              className="ui"
-              style={{
-                display: 'inline-flex',
-                border: '1px solid #000',
-                padding: '11px',
-                borderRadius: '15px',
-              }}
-            >
-              <span style={{ padding: '8px' }}>
-               MORE DETAILS
-              </span>
-            </div>
-          </Grid.Column>
-        </Grid>
+        {this.renderHeader()}
+        <Divider/>
         {this.renderTable()}
       </Segment>
     );
