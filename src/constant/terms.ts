9e3a730727cd9f0718b5f0c932e2a28faccade5a
Yüksel Ergüden <yerguden@gmail.com>
2019-08-15 03:20:20 +0200

    add form for step 1

---
+++ src/constant/terms.ts
@@ -0,0 +1,28 @@
+// todo: replace with correct keys values
+export default [
+  {
+    value: '1',
+    key: '1',
+    text: 'Paid when Order',
+  },
+  {
+    value: '2',
+    key: '2',
+    text: '30 Days',
+  },
+  {
+    value: '3',
+    key: '3',
+    text: '60 Days',
+  },
+  {
+    value: '4',
+    key: '4',
+    text: '90 Days',
+  },
+  {
+    value: '5',
+    key: '5',
+    text: 'custom',
+  },
+];
