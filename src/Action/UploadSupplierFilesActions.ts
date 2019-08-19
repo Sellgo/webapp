7aa9716459cd8946f6f20c0ac621462470c586f5
Yüksel Ergüden <yerguden@gmail.com>
2019-08-19 02:12:56 +0200

    add file select step

--- src/Action/UploadSupplierFilesActions.ts
+++ src/Action/UploadSupplierFilesActions.ts
@@ -1,4 +1,8 @@
-import { SET_UPLOAD_SUPPLIER_STEP } from '../constant/constant';
+import get from 'lodash/get';
+import { SET_UPLOAD_SUPPLIER_STEP, SET_CSV, SET_RAW_CSV } from '../constant/constant';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import parse from 'csv-parse/lib/es5/index';
 
 export const setUploadSupplierStep = (
   nextStep: number
@@ -9,3 +13,71 @@ export const setUploadSupplierStep = (
   type: SET_UPLOAD_SUPPLIER_STEP,
   payload: nextStep,
 });
+
+export const setRawCsv = (csvString: string | ArrayBuffer, csvFile: File) => ({
+  type: SET_RAW_CSV,
+  csvString,
+  csvFile,
+});
+
+export const setCsv = (csv: string) => ({
+  type: SET_CSV,
+  payload: csv,
+});
+
+export const parseCsv = () => (
+  dispatch: ThunkDispatch<{}, {}, AnyAction>,
+  getState: () => any
+): void => {
+  const rawString = get(getState(), 'uploadSupplierFiles.rawCsv');
+
+  if (!rawString) {
+    return;
+  }
+
+  const parseOption = {
+    trim: true,
+    relax: true,
+    relax_column_count: true,
+    // todo: switch to delimiter from state
+    delimiter: ',',
+  };
+
+  const getParsedCsv = (err: Error | undefined, output: string) => {
+    if (err) {
+      // tslint:disable-next-line:no-console
+      console.log({ err });
+    } else {
+      dispatch(setCsv(output));
+    }
+  };
+
+  // to ensure loader is visible delay execution by placing parse in async queue
+  Promise.resolve().then(() => {
+    parse(rawString, parseOption, getParsedCsv);
+  });
+};
+
+export const prepareCsv = (csvFile?: File) => async (
+  dispatch: ThunkDispatch<{}, {}, AnyAction>
+) => {
+  if (!csvFile) {
+    return;
+  }
+
+  const reader = new FileReader();
+  const statusFlag = 'csv_import';
+
+  reader.onloadend = () => {
+    const csvString = reader.result;
+
+    if (!csvString || reader.error) {
+      // handle error csv
+    } else {
+      dispatch(setRawCsv(csvString, csvFile));
+      dispatch(parseCsv());
+    }
+  };
+
+  reader.readAsText(csvFile);
+};
