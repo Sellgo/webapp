9f4d9eef817205089533354497020c119d48559a
Yüksel Ergüden <yerguden@gmail.com>
2019-08-28 04:52:16 +0300

    add icons to upload steps

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,24 +1,29 @@
 import 'rc-steps/assets/index.css';
 import 'rc-steps/assets/iconfont.css';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Steps, { Step } from 'rc-steps';
 
 export interface StepComponent
-  extends React.StatelessComponent<{ title: string; icon?: string; disabled?: boolean }> {}
+  extends React.StatelessComponent<{
+    title: string;
+    icon?: string | ReactNode;
+    disabled?: boolean;
+  }> {}
 
 export interface StepperProps {
   value?: number;
   onChange: (newValue: number) => void;
   children: (args: { Step: StepComponent }) => React.ReactNode;
+  className?: string;
 }
 
 export { Step };
 
 export default (props: StepperProps) => {
-  const { value = 0, onChange, children } = props;
+  const { value = 0, onChange, children, className } = props;
 
   return (
-    <Steps current={value} onChange={onChange}>
+    <Steps className={className} current={value} onChange={onChange}>
       {children({ Step })}
     </Steps>
   );
