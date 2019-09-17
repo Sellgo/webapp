1e01969c40a91a4a1c9c104d0e6390602a4135fd
rk <baisettr@oregonstate.edu>
2019-09-17 16:29:18 -0700

    https://sellgo.atlassian.net/browse/ME-268

---
+++ src/constant/userOnboarding.ts
@@ -0,0 +1,7 @@
+export const userOnboarding = [
+  {
+    title: 'Tutorial - Introduction',
+    description: 'How to use Sellgo platform',
+    url: 'https://www.youtube.com/embed/WoJ2-1V-aKU?rel=0',
+  },
+];
