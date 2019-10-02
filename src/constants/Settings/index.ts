5de911e681ec297d27e7f11fd74ae553e1ca679c
rk <baisettr@oregonstate.edu>
2019-10-02 15:21:12 -0700

    refactor Settings Page

--- src/constants/Settings/index.ts
+++ src/constants/Settings/index.ts
@@ -1,28 +1,29 @@
-import { MarketplaceFields } from '../../interfaces/MarketplaceFields';
-
-export const SET_BASIC_INFO_SELLER = 'SET_BASIC_INFO_SELLER';
-export const UPDATE_BASIC_INFO_SELLER = 'UPDATE_BASIC_INFO_SELLER';
-export const GET_BASIC_INFO_SELLER = 'GET_BASIC_INFO_SELLER';
-export const SET_AMAZON_MWS = 'SET_AMAZON_MWS';
-export const GET_AMAZON_MWS = 'GET_AMAZON_MWS';
-export const PATCH_AMAZON_MWS = 'PATCH_AMAZON_MWS';
-export const UPLOAD_SELLER_IMAGE = 'UPLOAD_SELLER_IMAGE';
+export const SET_SELLER_AMAZON_MWS_AUTH = 'SET_SELLER_AMAZON_MWS_AUTH';
+export const UPDATE_SELLER_AMAZON_MWS_AUTH = 'UPDATE_SELLER_AMAZON_MWS_AUTH';
+export const SET_SELLER_PROFILE_IMAGE = 'SET_SELLER_PROFILE_IMAGE';
+export const SET_SELLER_INFO = 'SET_SELLER_INFO';
+export const UPDATE_SELLER_INFO = 'UPDATE_SELLER_INFO';
+export const DELETE_SELLER_AMAZON_MWS_AUTH = 'DELETE_SELLER_AMAZON_MWS_AUTH';
 export const SET_PAGE_HISTORY_COUNTER = 'SET_PAGE_HISTORY_COUNTER';
 export const SET_PRICING_SUBSCRIPTIONS = 'SET_PRICING_SUBSCRIPTIONS';
 export const SET_SELLER_SUBSCRIPTION = 'SET_SELLER_SUBSCRIPTION';
 export const SET_AMAZON_MWS_AUTHORIZED = 'SET_AMAZON_MWS_AUTHORIZED';
 
-export const marketPlace: MarketplaceFields[] = [
-  { name: 'Brazil', id: 'A2Q3Y263D00KWC', code: 'BR', link: 'amazon.com' },
-  { name: 'Canada', id: 'A2EUQ1WTGCTBG2', code: 'CA', link: 'amazon.ca' },
-  { name: 'Mexico', id: 'A1AM78C64UM0Y8', code: 'MX', link: 'amazon.com.mx' },
-  { name: 'US', id: 'ATVPDKIKX0DER', code: 'US', link: 'amazon.com' },
-  { name: 'United Arab Emirates', id: 'A2VIGQ35RCS4UG', code: 'AE', link: 'amazon.ae' },
-  { name: 'Germany', id: 'A1PA6795UKMFR9', code: 'DE', link: 'amazon.de' },
-  { name: 'Spain', id: 'A1RKKUPIHCS9HS', code: 'ES', link: 'amazon.com' },
-  { name: 'France', id: 'A13V1IB3VIYZZH', code: 'FR', link: 'amazon.com' },
-  { name: 'UK', id: 'A1F83G8C2ARO7P', code: 'GB', link: 'amazon.com' },
-  { name: 'India', id: 'A21TJRUUN4KGV', code: 'IN', link: 'amazon.in' },
-  { name: 'Italy', id: 'APJ6JRA9NG5V4', code: 'IT', link: 'amazon.com' },
-  { name: 'Turkey', id: 'APJ6JRA9NG5V4', code: 'TR', link: 'amazon.com' },
+export const defaultMarketplaces = [
+  { name: 'Brazil', code: 'BR', link: 'amazon.com', id: 'A2Q3Y263D00KWC' },
+  { name: 'Canada', code: 'CA', link: 'amazon.ca', id: 'A2EUQ1WTGCTBG2' },
+  { name: 'Mexico', code: 'MX', link: 'amazon.com.mx', id: 'A1AM78C64UM0Y8' },
+  { name: 'US', code: 'US', link: 'amazon.com', id: 'ATVPDKIKX0DER' },
+  { name: 'United Arab Emirates (U.A.E.)', code: 'AE', link: 'amazon.ae', id: 'A2VIGQ35RCS4UG' },
+  { name: 'Germany', code: 'DE', link: 'amazon.de', id: 'A1PA6795UKMFR9' },
+  { name: 'Spain', code: 'ES', link: 'amazon.com', id: 'A1RKKUPIHCS9HS' },
+  { name: 'France', code: 'FR', link: 'amazon.com', id: 'A13V1IB3VIYZZH' },
+  { name: 'UK', code: 'GB', link: 'amazon.com', id: 'A1F83G8C2ARO7P' },
+  { name: 'India', code: 'IN', link: 'amazon.in', id: 'A21TJRUUN4KGV' },
+  { name: 'Italy', code: 'IT', link: 'amazon.com', id: 'APJ6JRA9NG5V4' },
+  { name: 'Turkey', code: 'TR', link: 'amazon.com', id: 'A33AVAJ2PDY3EV' },
+  { name: 'Singapore', code: 'SG', link: 'amazon.com', id: 'A19VAU5U5O7RUS' },
+  { name: 'Australia', code: 'AU', link: 'amazon.com.au', id: 'A39IBJ37TRP1C6' },
+  { name: 'Japan', code: 'JP', link: 'amazon.com', id: 'A1VC38T7YXB528' },
+  { name: 'China', code: 'CN', link: 'amazon.com', id: 'AAHKV2X7AFYLW' },
 ];
--- src/reducers/Settings/index.ts
+++ src/reducers/Settings/index.ts
@@ -1,127 +1,48 @@
 import {
-  SET_BASIC_INFO_SELLER,
-  UPDATE_BASIC_INFO_SELLER,
-  SET_AMAZON_MWS,
-  GET_BASIC_INFO_SELLER,
   SET_PAGE_HISTORY_COUNTER,
-  UPLOAD_SELLER_IMAGE,
-  GET_AMAZON_MWS,
-  PATCH_AMAZON_MWS,
+  SET_SELLER_AMAZON_MWS_AUTH,
   SET_AMAZON_MWS_AUTHORIZED,
+  UPDATE_SELLER_AMAZON_MWS_AUTH,
+  SET_SELLER_PROFILE_IMAGE,
+  SET_SELLER_INFO,
+  DELETE_SELLER_AMAZON_MWS_AUTH,
 } from '../../constants/Settings';
-import { Seller, AmazonMWS } from '../../interfaces/Seller';
 import { setIn } from '../../utils/immutablity';
+import get from 'lodash/get';
 
 const initialState = {
-  profile: {
-    name: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    id: '0',
-    cdate: '',
-  },
-  amazonMWS: {
-    seller_id: '',
-    amazon_seller_id: '',
-    marketplace_id: '',
-    token: '',
-    id: '',
-    status: '',
-  },
-  amazonMWSFromServer: [
-    {
-      seller_id: '',
-      amazon_seller_id: '',
-      marketplace_id: '',
-      token: '',
-      id: '',
-      status: '',
-    },
-  ],
+  profile: {},
+  amazonMWSAuth: [],
   pageHistoryCanGoForward: 0,
-  updatedImage: {},
-  success: false,
-  loading: false,
-  error: null,
-  isSideBarExpanded: false,
+  profileImage: {},
 };
 
 export default (state = initialState, action: any) => {
-  const newState = { ...state };
-  let data = null;
   switch (action.type) {
-    case SET_BASIC_INFO_SELLER:
-      data = action.data;
-      const key: keyof Seller = data.key;
-      const profile = { ...newState.profile };
-      profile[key] = data.value;
-      newState.profile = profile;
-      return newState;
-    case SET_AMAZON_MWS:
-      data = action.data;
-      const newKey: keyof AmazonMWS = data.key;
-      const amazonMWS = { ...newState.amazonMWS };
-      amazonMWS[newKey] = data.value;
-      newState.amazonMWS = amazonMWS;
-      return newState;
-    case UPDATE_BASIC_INFO_SELLER:
-      data = action.data;
-      newState.success = data.value;
-      return newState;
-    case GET_BASIC_INFO_SELLER:
-      const { name, cdate, id, email } = action.data;
-      const firstName = name ? name.substr(0, name.indexOf(' ')) : '';
-      const lastName = name ? name.substr(name.indexOf(' ') + 1) : '';
-      const sellerData = {
-        cdate,
-        id,
-        email,
-        firstName,
-        lastName,
-        name,
-      };
-      newState.profile = sellerData;
-      return newState;
-    case SET_PAGE_HISTORY_COUNTER:
-      data = action.data;
-      newState.pageHistoryCanGoForward = data;
-      return newState;
-    case UPLOAD_SELLER_IMAGE:
-      data = action.data;
-      newState.updatedImage = data;
-      return newState;
-    case GET_AMAZON_MWS:
-      data = action.data;
-      if (data.length > 0) {
-        // if (data[0].status !== 'inactive') {
-        newState.amazonMWSFromServer = data;
-        return newState;
-        // }
-      }
-      return state;
-    case PATCH_AMAZON_MWS:
-      // data = action.data;
-      data = {
-        seller_id: '',
-        amazon_seller_id: '',
-        marketplace_id: '',
-        token: '',
-        id: '',
-        status: '',
-      };
-      const amazonMWSfromServer = [...newState.amazonMWSFromServer];
-      for (const index in amazonMWSfromServer) {
-        if (amazonMWSfromServer[index].id === action.data.id) {
-          amazonMWSfromServer[index].status = 'inactive';
-        }
-      }
-      newState.amazonMWSFromServer = amazonMWSfromServer;
-      newState.amazonMWS = data;
-      return newState;
+    case SET_SELLER_AMAZON_MWS_AUTH:
+      return setIn(state, 'amazonMWSAuth', action.payload);
+
+    case DELETE_SELLER_AMAZON_MWS_AUTH:
+      return setIn(
+        state,
+        'amazonMWSAuth',
+        get(state, 'amazonMWSAuth').filter((mws: any) => mws.id !== action.payload)
+      );
+
+    case UPDATE_SELLER_AMAZON_MWS_AUTH:
+      return setIn(state, 'amazonMWSAuth', [...get(state, 'amazonMWSAuth'), action.payload]);
+
+    case SET_SELLER_PROFILE_IMAGE:
+      return setIn(state, 'profileImage', action.payload);
 
     case SET_AMAZON_MWS_AUTHORIZED:
       return setIn(state, 'amazonMWSAuthorized', action.payload);
+
+    case SET_SELLER_INFO:
+      return setIn(state, 'profile', { ...get(state, 'profile'), ...action.payload });
+
+    case SET_PAGE_HISTORY_COUNTER:
+      return setIn(state, 'pageHistoryCanGoForward', action.payload);
 
     default:
       return state;
