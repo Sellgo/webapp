a1f8f43ddde1010d05b13f1ffb987d5f94517c9c
Gabe Ragland <gabe.ragland@gmail.com>
2019-10-02 12:24:57 -0700

    Copy CTA component over to synthesis directory

---
+++ src/containers/Synthesis/Supplier/CallToAction/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Button, Progress } from 'semantic-ui-react';
+import './index.css';
+
+export default function CallToAction() {
+  return (
+    <div className="profitSysCallToAction">
+      <Progress percent={10} size="tiny">
+        80 tracked out of 100
+      </Progress>
+      <Button primary={true} className="add-new-supplier">
+        Upgrade
+      </Button>
+    </div>
+  );
+}
