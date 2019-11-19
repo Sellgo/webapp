8f19aa4c56b61564add954363f9ee52e44718789
vdarmadi <victor@sellgo.com>
2019-11-18 16:59:01 -0800

    Change error message

--- src/utils/validations/isPhone.js
+++ src/utils/validations/isPhone.js
@@ -1,4 +1,4 @@
 export const isNumber = value =>
   value && !/^\+?1 |(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/i.test(value)
-    ? 'Use US format'
+    ? 'Please use US phone number format'
     : undefined;
