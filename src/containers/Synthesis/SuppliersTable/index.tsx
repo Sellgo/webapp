bf48aae85952d02882877d223ca5cac7d2d50973
gaurav <dev.reactjs@gmail.com>
2019-12-06 13:27:12 +0530

    CEM-123 Changes

--- src/containers/Synthesis/SuppliersTable/index.tsx
+++ src/containers/Synthesis/SuppliersTable/index.tsx
@@ -398,4 +398,7 @@ const mapDispatchToProps = {
   deleteSupplier: (supplier: any) => deleteSupplier(supplier),
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SuppliersTable);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SuppliersTable);
