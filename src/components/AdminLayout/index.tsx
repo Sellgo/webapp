311194f47a757c046f2dfa15789126e0bedcdf8b
unknown <iamnaj999@gmail.com>
2020-02-15 08:47:52 +0800

    remove aspect ratio functionality

--- src/components/AdminLayout/index.tsx
+++ src/components/AdminLayout/index.tsx
@@ -8,52 +8,15 @@ interface Props {
   auth: Auth;
 }
 
-interface State {
-  ratio: string;
-}
-
 class AdminLayout extends React.Component<Props> {
-  state: State = {
-    ratio: '',
-  };
-
-  constructor(props: any) {
-    super(props);
-    this.updatePredicate = this.updatePredicate.bind(this);
-  }
-
-  componentDidMount() {
-    this.updatePredicate();
-    window.addEventListener('resize', this.updatePredicate);
-  }
-
-  componentWillUnmount() {
-    this.updatePredicate();
-    window.removeEventListener('resize', this.updatePredicate);
-  }
-
-  updatePredicate() {
-    let c = window.innerWidth / window.innerHeight;
-    let newRatio =
-      c > 1.6
-        ? 'screen-16-9'
-        : c <= 1.6 && c > 1.34
-        ? 'screen-16-10'
-        : c < 1.34 && c >= 1
-        ? 'screen-4-3'
-        : c < 1
-        ? 'mobile'
-        : '';
-    this.setState({ ratio: newRatio });
-  }
-
   public render() {
     const { children, auth } = this.props;
-    const { ratio } = this.state;
+
     return (
       <React.Fragment>
         <AdminHeader auth={auth} children={this.props.children} />
-        <Segment className={'admin-layout ' + ratio} basic={true}>
+
+        <Segment className="admin-layout" basic={true}>
           {children}
         </Segment>
       </React.Fragment>
