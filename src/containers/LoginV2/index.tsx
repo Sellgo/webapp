d12c5472f750f7747bd039ded8e18a8d96db5140
Marlon Hario <hariomarlon83@gmail.com>
2020-02-21 20:22:55 +0800

    Style for wide screen responsiveness and apply TSLint.

--- src/containers/LoginV2/index.tsx
+++ src/containers/LoginV2/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import { 
-  Button, 
-  Form, 
-  Checkbox } from "semantic-ui-react";
+import { Button, Form, Checkbox } from 'semantic-ui-react';
 import LoginBase from '../../components/LoginBase';
 import './index.scss';
 
@@ -10,21 +7,19 @@ export default class LoginV2 extends React.Component<any> {
   render() {
     return (
       <LoginBase>
-        <Form>
-          <Form.Input
-            label="Username"
-            type="mail"
-            placeholder="name@domain.com"
-          />
+        <Form className="login-form">
+          <Form.Input label="Username" type="mail" placeholder="name@domain.com" />
           <Form.Input label="Password" type="password" />
-          <Form.Group inline>
+          <Form.Group inline={true}>
             <Form.Field control={Checkbox} label="Remember me" />
             <a href="#"> Forgot password </a>
           </Form.Group>
-          <Form.Field control={Button} fluid primary>
+          <Form.Field control={Button} fluid={true} primary={true}>
             Log in
           </Form.Field>
-          <a className="sign-up" href="#"> Sign up for an account </a>
+          <a className="sign-up" href="#">
+            <b>Sign up for an account</b>
+          </a>
         </Form>
       </LoginBase>
     );
