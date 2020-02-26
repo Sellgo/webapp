40a6cebae6dec03d1bb611acbaa5082c7f024f2f
Jitro <jjuanndelacruz@gmail.com>
2020-02-26 09:21:30 -0800

    upload supplier modal ui changes

---
+++ src/containers/Synthesis/UploadSupplier/AddNewSearch.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Form, Grid, TextArea, Popup, Icon } from 'semantic-ui-react';
+import styles from './UploadSupplier.module.css';
+import { Field } from 'redux-form';
+import { InputField, SelectField, TextAreaField } from '../../../components/ReduxFormFields';
+import { defaultSelect } from '../../../constants';
+import timezones from '../../../constants/UploadSupplier/timezones';
+import { accountStatus } from '../../../constants/UploadSupplier';
+import { terms } from '../../../constants/UploadSupplier';
+import isRequired from '../../../utils/validations/isRequired';
+import { isNumber } from '../../../utils/validations/isPhone';
+import isName from '../../../utils/validations/isName.js';
+import { onlyNumber } from '../../../utils/validations/isOnlyNumber';
+import { webUrl } from '../../../utils/validations/isUrl';
+
+const required = isRequired();
+
+const SupplierDetails = () => (
+  <React.Fragment>
+    <Grid.Column className={styles['rightAdjust']} width={7}>
+      <Field
+        className={styles['dropdwn-wdth']}
+        component={SelectField}
+        name="marketPlace"
+        label="Market Place"
+        placeholder="Amazon.com"
+        options={[defaultSelect, ...accountStatus]}
+      />
+      <Grid columns="equal">
+        <Grid.Column className={styles['taxAlign']}>
+          <Field
+            component={InputField}
+            name="salesTax"
+            label="Sales Tax (%)"
+            placeholder="%"
+            className={styles['field-width']}
+            type="number"
+            min="0"
+            validate={onlyNumber}
+          />
+        </Grid.Column>
+        <Grid.Column>
+          <Field
+            component={InputField}
+            name="prepUnit"
+            label="Prep $/Unit ($)"
+            placeholder="$"
+            className={styles['field-width']}
+            type="number"
+            min="0"
+            validate={onlyNumber}
+          />
+        </Grid.Column>
+      </Grid>
+    </Grid.Column>
+  </React.Fragment>
+);
+
+const AddNewSearch = () => {
+  return (
+    <div className={styles['ouline-box']}>
+      <Form
+        className={styles['supply-container']}
+        style={{ maxWidth: '700px', width: '100%', paddingBottom: '80px' }}
+      >
+        <Grid columns="equal" className="bg-color">
+          <Grid.Column width={7} className={`${styles.padding0} ${styles['leftAdjust']}`}>
+            <div className={styles['form-container']}>
+              <Field
+                required={true}
+                validate={required}
+                component={InputField}
+                name="searchName"
+                label="Search Name"
+                placeholder="Search Name"
+                maxLength="100"
+              />
+              <div className={`field ${styles['description-box']}`}>
+                <label>Search Description</label>
+                <TextArea
+                  name="searchDescription"
+                  label="Search Description"
+                  placeholder="Search Description"
+                  style={{ minHeight: 70 }}
+                />
+              </div>
+            </div>
+          </Grid.Column>
+          {<SupplierDetails />}
+        </Grid>
+      </Form>
+    </div>
+  );
+};
+
+export default AddNewSearch;
--- src/containers/Synthesis/UploadSupplier/index.tsx
+++ src/containers/Synthesis/UploadSupplier/index.tsx
@@ -11,6 +11,7 @@ import Submit from './Submit';
 import FormWrapper from './FormWrapper';
 import { cleanupUploadSupplier } from '../../../actions/UploadSupplier';
 import DataValidation from './DataValidation';
+import AddNewSearch from './AddNewSearch';
 
 interface Props {
   currentStep: number;
@@ -34,15 +35,17 @@ export const UploadSupplier = (props: Props) => {
       <div className={styles['margin-top']} />
       <div className={styles.section}>
         <FormWrapper>
-          {currentStep === 0 && <SupplierInformation />}
+          {currentStep === 0 && <AddNewSearch />}
 
-          {currentStep === 1 && <SelectFile />}
+          {currentStep === 1 && <SupplierInformation />}
 
-          {currentStep === 2 && <DataMapping />}
+          {currentStep === 2 && <SelectFile />}
 
-          {currentStep === 3 && <DataValidation />}
+          {currentStep === 3 && <DataMapping />}
 
-          {currentStep === 4 && (
+          {currentStep === 4 && <DataValidation />}
+
+          {currentStep === 5 && (
             <Submit
               onFinished={() => {
                 setFinished(true);
