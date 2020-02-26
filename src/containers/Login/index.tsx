82e897c579055bb0ccd40d71948dcc3209181234
unknown <iamnaj999@gmail.com>
2020-02-27 04:42:28 +0800

    New login( initial )

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,27 +1,50 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, Form, Checkbox } from 'semantic-ui-react';
 import LoginBase from '../../components/LoginBase';
+import { useInput } from '../../hooks/useInput';
 import './index.scss';
+import Auth from '../../components/Auth/Auth';
 
-export default class Login extends React.Component<any> {
-  render() {
-    return (
-      <LoginBase>
-        <Form className="login-form">
-          <Form.Input label="Username" type="mail" placeholder="name@domain.com" />
-          <Form.Input label="Password" type="password" />
-          <Form.Group inline={true}>
-            <Form.Field control={Checkbox} label="Remember me" />
-            <a href="#"> Forgot password </a>
-          </Form.Group>
-          <Form.Field control={Button} fluid={true} primary={true}>
-            Log in
-          </Form.Field>
-          <a className="sign-up" href="#">
-            <b>Sign up for an account</b>
-          </a>
-        </Form>
-      </LoginBase>
+interface Props {
+  auth: Auth;
+}
+export default function Login(props: Props) {
+  const { auth } = props;
+  const { value: username, bind: bindUserName } = useInput('');
+  const { value: password, bind: bindPassword } = useInput('');
+
+  const handleSubmit = () => {
+    auth.webAuth.login(
+      {
+        responseType: 'token',
+        realm: 'Username-Password-Authentication',
+        username: username,
+        password: password,
+      },
+      (err: any, res: any) => {
+        if (err) {
+          console.log('Error: ', err);
+        }
+      }
     );
-  }
+  };
+
+  return (
+    <LoginBase>
+      <Form className="login-form" onSubmit={handleSubmit}>
+        <Form.Input label="Username" type="mail" placeholder="name@domain.com" {...bindUserName} />
+        <Form.Input label="Password" type="password" {...bindPassword} />
+        <Form.Group inline={true}>
+          <Form.Field control={Checkbox} label="Remember me" />
+          <a href="#"> Forgot password </a>
+        </Form.Group>
+        <Form.Field control={Button} fluid={true} primary={true} value="Submit">
+          Log in
+        </Form.Field>
+        <a className="sign-up" href="#">
+          <b>Sign up for an account</b>
+        </a>
+      </Form>
+    </LoginBase>
+  );
 }
