33926015054ff8e7fa7992cdb197af0089f1a609
unknown <iamnaj999@gmail.com>
2020-03-03 01:20:49 +0800

    done but needs refactoring

--- src/components/MessageDisplay/index.tsx
+++ src/components/MessageDisplay/index.tsx
@@ -2,47 +2,43 @@ import React from 'react';
 import { Rail, Segment, Message } from 'semantic-ui-react';
 import './index.scss';
 
-interface State {
-  setMessageContent: any;
-}
-interface Props {
-  header: string;
-  content: string;
-  time: number;
-}
-
-export default class MessageDisplay extends React.Component<any> {
-  // setMessage = (header:string, content:string) => {
-  //     return(
-  //         <Message
-  //         success={true}
-  //         header={header}
-  //         content={content}
-  //     />
-  //     )
-  // }
-  render() {
-    // const { header, content, time } = this.props;
-    return (
-      <Rail internal={true} position="left">
-        <Segment>
-          <Message
-            success={true}
-            header={'Account Created'}
-            content={'A link to verify your email has been sent to'}
-          />
-        </Segment>
-      </Rail>
-    );
-  }
+export default function MessageDisplay(props: any) {
+  return (
+    <Rail internal={true} position="left">
+      <Segment id="segment-container"></Segment>
+    </Rail>
+  );
 }
 
-//  export function setMessage () {
-//     return(
-//         <Message
-//             success={true}
-//             header={header}
-//             content={content}
-//         />
-//     )
-// }
+export function setMessage(header: string, message: string, type: string, time: number) {
+  // Need to be refactored cause I can't figure out the way to pushed it to the parent element <Segment></Segment> on above code
+  /*
+    The final code would be something like this
+    <Message 
+        success={type}
+        error={type}
+        header={header}
+        content={message}
+    />
+  */
+
+  const segmentContainer = document.getElementById('segment-container') as HTMLElement;
+  const messageContainer = document.createElement('div');
+  const messageContent = document.createElement('div');
+  const headerContent = document.createElement('div');
+  const paragraph = document.createElement('p');
+  messageContainer.id = 'messageContainer';
+  messageContainer.className = `ui ${type} message`;
+  messageContent.className = 'content';
+  headerContent.className = 'header';
+  segmentContainer.appendChild(messageContainer);
+  messageContainer.appendChild(messageContent);
+  headerContent.innerHTML = header;
+  paragraph.innerHTML = message;
+  messageContent.appendChild(headerContent);
+  messageContent.appendChild(paragraph);
+
+  setTimeout(() => {
+    messageContainer.remove();
+  }, time);
+}
