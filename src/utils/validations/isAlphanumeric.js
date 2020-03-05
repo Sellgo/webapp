480414f540228212b01edce33b6dd3c6d4ad93c0
vdarmadi <victor@sellgo.com>
2020-03-05 08:18:21 -0800

    Test failing lint

--- src/utils/validations/isAlphanumeric.js
+++ src/utils/validations/isAlphanumeric.js
@@ -1,6 +1,7 @@
 import isAlphanumeric from 'validator/lib/isAlphanumeric';
+import { t } from 'i18next';
 
-export default (errorMessage = 'Alphanumeric error') => value => {
+export default (errorMessage = t('common.alphanumericError')) => value => {
   if (value && !isAlphanumeric(value)) return errorMessage;
 
   return undefined;
