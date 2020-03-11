254ca67daaaac7408b0024575bd574159752b584
Marlon Hario <hariomarlon83@gmail.com>
2020-03-12 05:43:41 +0800

    Remove pricing UI component and route.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -6,7 +6,6 @@ import ScrollToTop from '../../components/ScrollToTop';
 import Settings from '../Settings';
 import Subscription from '../Settings/Subscription';
 import Home from '../Home';
-import Pricing from '../Pricing';
 import Synthesis from '../Synthesis';
 import SupplierDetail from '../Synthesis/Supplier';
 import Auth from '../../components/Auth/Auth';
@@ -174,7 +173,6 @@ function App() {
             component={ProductTracker}
             requireSubscription={true}
           />
-          <Route exact={true} path="/pricing" component={Pricing} />
           <Route component={NotFound} />
         </Switch>
       </Router>
