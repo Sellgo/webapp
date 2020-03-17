a206da963675b315e34fa34047165ae4ca8f229b
iamnaj1 <iamnaj999@gmail.com>
2020-03-18 07:59:53 +0800

    update

--- src/components/FilterContainer/index.tsx
+++ src/components/FilterContainer/index.tsx
@@ -3,188 +3,31 @@ import './index.scss';
 import { Checkbox, Radio, Button } from 'semantic-ui-react';
 import _ from 'lodash';
 import { FilterData } from '../../interfaces/Filters';
+import InputRange from 'react-input-range';
 
 interface Props {
   filterType: string;
+  setRadioFilter: (filterType: string, value: string) => void;
+  toggleCheckboxFilter: (filterType: string, filterDataKey: string) => void;
+  applyFilter: () => void;
+  resetPrice: () => void;
+  allFilters: FilterData[];
 }
+
 function FilterContainer(props: Props) {
-  const { filterType } = props;
-  const filterData: FilterData[] = [
-    {
-      label: 'Sort By',
-      dataKey: 'sort-by',
-      checkedValue: 'most-profitable',
-      radio: true,
-      data: [
-        {
-          label: 'Most Profitable',
-          dataKey: 'most-profitable',
-        },
-        {
-          label: 'Most Units Sold',
-          dataKey: 'most-unit-sold',
-        },
-        {
-          label: 'Highest ROI',
-          dataKey: 'highest-roi',
-        },
-      ],
-    },
-    {
-      label: 'Sellers',
-      dataKey: 'seller',
-      radio: false,
-      data: [
-        {
-          label: 'Amazon',
-          dataKey: 'amazon',
-          checked: true,
-        },
-        {
-          label: 'FBA',
-          dataKey: 'fba',
-          checked: false,
-          childData: [
-            {
-              label: '> 3 FBA',
-              dataKey: '3fba',
-              checked: false,
-            },
-          ],
-        },
-        {
-          label: 'FBM',
-          dataKey: 'fbm',
-          checked: false,
-        },
-      ],
-    },
-    {
-      label: 'Sellability',
-      dataKey: 'sellability',
-      radio: false,
-      data: [
-        {
-          label: 'Non-Hazmat',
-          dataKey: 'non-hazmat',
-          checked: true,
-        },
-        {
-          label: 'Non-Gated',
-          dataKey: 'non-gated',
-          checked: false,
-        },
-      ],
-    },
-    {
-      label: 'Product Category',
-      dataKey: 'product-category',
-      radio: false,
-      data: [
-        {
-          label: 'All',
-          dataKey: 'all-products',
-          checked: true,
-        },
-        {
-          label: 'Appliances',
-          dataKey: 'appliances',
-          checked: false,
-        },
-        {
-          label: 'Arts, Craft and Sewing',
-          dataKey: 'arts-craft-sewing',
-          checked: false,
-        },
-        {
-          label: 'Baby',
-          dataKey: 'baby',
-          checked: false,
-        },
-        {
-          label: 'Books',
-          dataKey: 'books',
-          checked: false,
-        },
-      ],
-    },
-    {
-      label: 'Dimensions',
-      dataKey: 'dimension',
-      checkedValue: 'small-size',
-      radio: true,
-      data: [
-        {
-          label: 'Small Size',
-          dataKey: 'small-size',
-        },
-        {
-          label: 'Medium Size',
-          dataKey: 'medium-size',
-        },
-        {
-          label: 'Over Size',
-          dataKey: 'over-size',
-        },
-      ],
-    },
-    {
-      label: 'Weight',
-      dataKey: 'weight',
-      checkedValue: 'light-weight',
-      radio: true,
-      data: [
-        {
-          label: 'Light Weight',
-          dataKey: 'light-weight',
-        },
-        {
-          label: 'Medium Weight',
-          dataKey: 'medium-weight',
-        },
-        {
-          label: 'Over Weight',
-          dataKey: 'over-weight',
-        },
-      ],
-    },
-  ];
-
-  const [allFilters, setAllFilters] = React.useState(filterData);
-
-  const setRadioFilter = (filterType: string, value: string) => {
-    const data = _.map(allFilters, filter => {
-      if (filter.dataKey === filterType) {
-        filter.checkedValue = value;
-      }
-      return filter;
-    });
-    setAllFilters(data);
-  };
-
-  const toggleCheckboxFilter = (filterType: string, filterDataKey: string) => {
-    const filter = _.map(allFilters, filter => {
-      if (filter.dataKey === filterType) {
-        const filterData = _.map(filter.data, filterData => {
-          if (filterData.dataKey === filterDataKey) {
-            filterData.checked = !filterData.checked;
-          }
-        });
-      }
-      return filter;
-    });
-    setAllFilters(filter);
-  };
-
-  const applyFilter = () => {
-    console.log('filterData: ', allFilters);
-  };
+  const {
+    filterType,
+    allFilters,
+    applyFilter,
+    setRadioFilter,
+    toggleCheckboxFilter,
+    resetPrice,
+  } = props;
 
   return (
     <div className="filter-container">
       {filterType === 'all-filter' && (
         <>
-          {' '}
           <div className="all-filter-content-wrapper">
             {_.map(allFilters, (filter, key) => {
               return (
@@ -240,6 +83,59 @@ function FilterContainer(props: Props) {
                 </div>
               );
             })}
+          </div>
+          <div className="button-wrapper">
+            <Button basic className="apply-filter-btn" onClick={() => applyFilter()}>
+              Apply
+            </Button>
+          </div>
+        </>
+      )}
+
+      {filterType === 'price-filter' && (
+        <>
+          <div className="price-filter-content-wrapper">
+            <div className="price-range-container">
+              <h3>Price $</h3>
+              <span className="reset" onClick={() => resetPrice()}>
+                x Reset
+              </span>
+              <div className="price-range-content">
+                <InputRange
+                  step={0.01}
+                  minValue={0}
+                  maxValue={20}
+                  value={{ min: 2, max: 10 }}
+                  onChange={value => {
+                    console.log('value: ', value);
+                  }}
+                />
+                <div className="min-max-content">
+                  <span>Min</span>
+                  <span>Max</span>
+                </div>
+              </div>
+            </div>
+            <div className="price-details-content">
+              <div className="price-min items">
+                <span>Products below min price</span>
+                <div className="min-count">
+                  <span>1000</span> items
+                </div>
+              </div>
+              <div className="price-range items">
+                <span>Products in the range price</span>
+                <div className="in-range-count">
+                  <span>1000</span> items
+                </div>
+              </div>
+              <div className="price-max items">
+                <span>Products above max price</span>
+                <div className="max-count">
+                  <span>1000</span> items
+                </div>
+              </div>
+            </div>
           </div>
           <div className="button-wrapper">
             <Button basic className="apply-filter-btn" onClick={() => applyFilter()}>
--- src/components/FilterSection2/index.tsx
+++ src/components/FilterSection2/index.tsx
@@ -3,18 +3,20 @@ import './index.scss';
 import { Button, Icon } from 'semantic-ui-react';
 import _ from 'lodash';
 import FilterContainer from '../FilterContainer';
+import { FilterData } from '../../interfaces/Filters';
 
 interface FilterObject {
   label: string;
   dataKey: string;
 }
 interface State {
-  filterData: FilterObject[];
+  filterTitle: FilterObject[];
 }
 
 function FilterSection2(props: any, state: State) {
+  const [filterType, setFilterType] = useState('');
   state = {
-    filterData: [
+    filterTitle: [
       {
         label: 'Time',
         dataKey: 'time-filter',
@@ -34,8 +36,180 @@ function FilterSection2(props: any, state: State) {
     ],
   };
 
-  const [filterType, setFilterType] = useState('');
+  const filterData: FilterData[] = [
+    {
+      label: 'Sort By',
+      dataKey: 'sort-by',
+      checkedValue: 'most-profitable',
+      radio: true,
+      data: [
+        {
+          label: 'Most Profitable',
+          dataKey: 'most-profitable',
+        },
+        {
+          label: 'Most Units Sold',
+          dataKey: 'most-unit-sold',
+        },
+        {
+          label: 'Highest ROI',
+          dataKey: 'highest-roi',
+        },
+      ],
+    },
+    {
+      label: 'Sellers',
+      dataKey: 'seller',
+      radio: false,
+      data: [
+        {
+          label: 'Amazon',
+          dataKey: 'amazon',
+          checked: true,
+        },
+        {
+          label: 'FBA',
+          dataKey: 'fba',
+          checked: false,
+          childData: [
+            {
+              label: '> 3 FBA',
+              dataKey: '3fba',
+              checked: false,
+            },
+          ],
+        },
+        {
+          label: 'FBM',
+          dataKey: 'fbm',
+          checked: false,
+        },
+      ],
+    },
+    {
+      label: 'Sellability',
+      dataKey: 'sellability',
+      radio: false,
+      data: [
+        {
+          label: 'Non-Hazmat',
+          dataKey: 'non-hazmat',
+          checked: true,
+        },
+        {
+          label: 'Non-Gated',
+          dataKey: 'non-gated',
+          checked: false,
+        },
+      ],
+    },
+    {
+      label: 'Product Category',
+      dataKey: 'product-category',
+      radio: false,
+      data: [
+        {
+          label: 'All',
+          dataKey: 'all-products',
+          checked: true,
+        },
+        {
+          label: 'Appliances',
+          dataKey: 'appliances',
+          checked: false,
+        },
+        {
+          label: 'Arts, Craft and Sewing',
+          dataKey: 'arts-craft-sewing',
+          checked: false,
+        },
+        {
+          label: 'Baby',
+          dataKey: 'baby',
+          checked: false,
+        },
+        {
+          label: 'Books',
+          dataKey: 'books',
+          checked: false,
+        },
+      ],
+    },
+    {
+      label: 'Dimensions',
+      dataKey: 'dimension',
+      checkedValue: 'small-size',
+      radio: true,
+      data: [
+        {
+          label: 'Small Size',
+          dataKey: 'small-size',
+        },
+        {
+          label: 'Medium Size',
+          dataKey: 'medium-size',
+        },
+        {
+          label: 'Over Size',
+          dataKey: 'over-size',
+        },
+      ],
+    },
+    {
+      label: 'Weight',
+      dataKey: 'weight',
+      checkedValue: 'light-weight',
+      radio: true,
+      data: [
+        {
+          label: 'Light Weight',
+          dataKey: 'light-weight',
+        },
+        {
+          label: 'Medium Weight',
+          dataKey: 'medium-weight',
+        },
+        {
+          label: 'Over Weight',
+          dataKey: 'over-weight',
+        },
+      ],
+    },
+  ];
 
+  const [allFilters, setAllFilters] = React.useState(filterData);
+
+  const setRadioFilter = (filterType: string, value: string) => {
+    const data = _.map(allFilters, filter => {
+      if (filter.dataKey === filterType) {
+        filter.checkedValue = value;
+      }
+      return filter;
+    });
+    setAllFilters(data);
+  };
+
+  const toggleCheckboxFilter = (filterType: string, filterDataKey: string) => {
+    const filter = _.map(allFilters, filter => {
+      if (filter.dataKey === filterType) {
+        const filterData = _.map(filter.data, filterData => {
+          if (filterData.dataKey === filterDataKey) {
+            filterData.checked = !filterData.checked;
+          }
+        });
+      }
+      return filter;
+    });
+    setAllFilters(filter);
+  };
+
+  const resetPrice = () => {
+    console.log('resetPrice: ');
+  };
+
+  const applyFilter = () => {
+    console.log('filterData: ', allFilters);
+  };
   return (
     <div className="filter-section">
       <div className="filter-header">
@@ -50,7 +224,7 @@ function FilterSection2(props: any, state: State) {
           <span className="filter-count">1</span>
           <Icon className="slider" name="sliders horizontal" />
         </Button>
-        {_.map(state.filterData, filter => {
+        {_.map(state.filterTitle, filter => {
           return (
             <Button
               basic
@@ -71,7 +245,14 @@ function FilterSection2(props: any, state: State) {
       </div>
       <div className="filter-wrapper">
         <hr />
-        <FilterContainer filterType={filterType} />
+        <FilterContainer
+          filterType={filterType}
+          applyFilter={applyFilter}
+          setRadioFilter={setRadioFilter}
+          toggleCheckboxFilter={toggleCheckboxFilter}
+          resetPrice={resetPrice}
+          allFilters={allFilters}
+        />
       </div>
     </div>
   );
