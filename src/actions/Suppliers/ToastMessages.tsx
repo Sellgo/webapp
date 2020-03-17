3fbb7a7c5ef3f4008a2cd6cc5cc4f4a5493d3929
Marcus Ong <marcus.ong404@gmail.com>
2020-03-17 14:17:49 -0700

    change toast message

---
+++ src/actions/Suppliers/ToastMessages.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const UntrackSuccess = (
+  <>
+    <h3>Product Successfully Untracked</h3>
+    <p>You may add a new product through profit finder</p>
+  </>
+);
