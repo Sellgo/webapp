95c7588af1da5e7739494e89a0778ecca10ee394
iamnaj1 <iamnaj999@gmail.com>
2020-04-08 07:11:48 +0800

    initial

---
+++ src/containers/ProductTracker/ProductTrackerFilterSection/index.tsx
@@ -0,0 +1,13 @@
+// import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+// import get from 'lodash/get';
+// import _ from 'lodash';
+function ProductTrackerFilterSection() {
+  return null;
+}
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductTrackerFilterSection);
