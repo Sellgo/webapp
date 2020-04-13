567ddb434b6b13e41b7b5fc3257f0ba83bf36ee4
iamnaj1 <iamnaj999@gmail.com>
2020-04-14 00:09:33 +0800

    Number representation in Monthly revenue

--- src/containers/Synthesis/Supplier/ProductsTable/index.tsx
+++ src/containers/Synthesis/Supplier/ProductsTable/index.tsx
@@ -90,7 +90,9 @@ class ProductsTable extends React.Component<ProductsTableProps> {
   renderProfit = (row: Product) => <p className="stat">{formatCurrency(row.profit)}</p>;
   renderMargin = (row: Product) => <p className="stat">{row.margin}%</p>;
   renderFee = (row: Product) => <p className="stat">{row.fees}%</p>;
-  renderMonthlyRevenue = (row: Product) => <p className="stat">${row.monthly_revenue}</p>;
+  renderMonthlyRevenue = (row: Product) => (
+    <p className="stat">${formatNumber(row.monthly_revenue)}</p>
+  );
   renderRoi = (row: Product) => <p className="stat">{row.roi}%</p>;
   renderRank = (row: Product) => <p className="stat">#{formatNumber(row.rank)}</p>;
   renderMonthlySalesEst = (row: Product) => {
