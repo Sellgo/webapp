cf81c54e57cac11196c891e9eb28aad3b5a7a0cb
Marcus Ong <marcus.ong404@gmail.com>
2020-04-30 15:10:51 +0800

    change cursor pointer

--- src/containers/ProductTracker/ProductTrackerTable/index.tsx
+++ src/containers/ProductTracker/ProductTrackerTable/index.tsx
@@ -230,7 +230,7 @@ class ProductTrackerTable extends React.Component<TrackerProps> {
     return (
       <div className="avg-margin">
         <p className="stat">{row.avg_margin !== '0.00' ? `${row.avg_margin}%` : 'N.A.'}</p>
-        <span className="caret-icon">
+        <span className="caret-icon" style={{ cursor: 'pointer' }}>
           <Icon className="caret down" onClick={() => toggleExpandRow(row.product_id)} />
         </span>
       </div>
