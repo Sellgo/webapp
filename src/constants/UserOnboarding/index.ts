e20a999e8c5d862c0ea548e09458816bc6cc24b8
Ingyu Woo <ingyu@sellgo.com>
2020-05-08 11:51:59 -0700

    update onboarding videos

--- src/constants/UserOnboarding/index.ts
+++ src/constants/UserOnboarding/index.ts
@@ -3,8 +3,18 @@ export const SET_PP = 'SET_PP';
 
 export const userOnboarding = [
   {
-    title: 'Tutorial - Introduction',
-    description: 'How to use Sellgo platform',
-    url: 'https://www.youtube.com/embed/WoJ2-1V-aKU?rel=0',
+    title: 'Supplier Management Tutorial',
+    description: 'How to use Supplier Management',
+    url: 'https://www.youtube.com/embed/-CpCzUP0AVQ',
+  },
+  {
+    title: 'Profit Finder Tutorial',
+    description: 'How to use Profit Finder',
+    url: 'https://www.youtube.com/embed/C-bpxF0c-24',
+  },
+  {
+    title: 'Product Tracker Tutorial',
+    description: 'How to use Product Tracker',
+    url: 'https://www.youtube.com/embed/WNAVfz7Ss-A',
   },
 ];
