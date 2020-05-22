91d55203f730771dfb616dad7f7c7e81a9edaa9a
Marcus Ong <marcus.ong404@gmail.com>
2020-05-22 13:11:24 +0800

    add mws auth check to bulk track/untrack

--- src/actions/Suppliers/index.ts
+++ src/actions/Suppliers/index.ts
@@ -461,7 +461,9 @@ export const requestProductBulkTracking = (products: { product_id: number }[]) =
     })
     .catch(err => {
       console.log('err.response', err.response);
-      if (err.response && (err.response.status !== 200 || err.response.status !== 201)) {
+      if (err.response && err.response.status === 401) {
+        handleUnauthorizedMwsAuth();
+      } else if (err.response && (err.response.status !== 200 || err.response.status !== 201)) {
         error(err.response.data.message);
       }
     });
@@ -485,7 +487,9 @@ export const requestProductBulkUnTracking = (products: { product_id: number }[])
     })
     .catch(err => {
       console.log('err.response', err.response);
-      if (err.response && err.response.status !== 200) {
+      if (err.response && err.response.status === 401) {
+        handleUnauthorizedMwsAuth();
+      } else if (err.response && err.response.status !== 200) {
         error(err.response.data.message);
       }
     });
