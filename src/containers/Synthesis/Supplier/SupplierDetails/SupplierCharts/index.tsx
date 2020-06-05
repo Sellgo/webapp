6d91fa1aa5f1218b8b02cdb90d633b5d5663ef0d
Marcus Ong <marcus.ong404@gmail.com>
2020-06-05 15:13:38 +0800

    only show arrows on revenue chart

--- src/containers/Synthesis/Supplier/SupplierDetails/SupplierCharts/index.tsx
+++ src/containers/Synthesis/Supplier/SupplierDetails/SupplierCharts/index.tsx
@@ -91,12 +91,14 @@ class SupplierCharts extends Component<SupplierChartsProps> {
       <div className="supplier-charts">
         <Grid className="supplier-charts__chart-grid">
           <div className="chart-grid__left-column">
-            <Icon
-              className="chart-grid__left-arrow"
-              name="angle left"
-              size="big"
-              onClick={this.handleLeftArrowClick}
-            />
+            {this.state.showChart === 'chart1' && (
+              <Icon
+                className="chart-grid__left-arrow"
+                name="angle left"
+                size="big"
+                onClick={this.handleLeftArrowClick}
+              />
+            )}
           </div>
           <div className="chart-grid__middle-column">
             {/* IMPORTANT: these inner divs & styles are required to handle chart resizing on window resize */}
@@ -108,12 +110,14 @@ class SupplierCharts extends Component<SupplierChartsProps> {
             </div>
           </div>
           <div className="chart-grid__right-column">
-            <Icon
-              className="chart-grid__right-arrow"
-              name="angle right"
-              size="big"
-              onClick={this.handleRightArrowClick}
-            />
+            {this.state.showChart === 'chart1' && (
+              <Icon
+                className="chart-grid__right-arrow"
+                name="angle right"
+                size="big"
+                onClick={this.handleRightArrowClick}
+              />
+            )}
           </div>
         </Grid>
         <Grid centered className="chart-end-content">
