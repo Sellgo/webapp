41a7d87f46d2a7d2772b1d4a3f23add06d7e426c
Marcus Ong <marcus.ong404@gmail.com>
2020-06-19 23:02:52 +0800

    add 'other' market share

--- src/containers/Synthesis/Supplier/ProductDetails/ProductCharts/MarketShareChart/index.tsx
+++ src/containers/Synthesis/Supplier/ProductDetails/ProductCharts/MarketShareChart/index.tsx
@@ -10,15 +10,32 @@ export enum SHOW_TYPE {
   SellerLevelInventory,
 }
 
+interface Series {
+  yAxis: number;
+  type: string;
+  name: string;
+  data: [number, number][];
+  totalValue?: number;
+  color: string;
+  step?: boolean;
+  zIndex?: number;
+}
+
 export default ({
   productRanks,
   productInventories,
   sellerInventories,
-  currentShowType = SHOW_TYPE.SumOfSellerLevelInventory,
-}: any) => {
+  currentShowType = SHOW_TYPE.ProductLevelInventory,
+}: {
+  productRanks: [number, number][];
+  productInventories: [number, number][];
+  sellerInventories: { [key: string]: { name: string; data: [number, number][]; color: string } };
+  currentShowType?: SHOW_TYPE;
+}) => {
   const showRanks = true;
-  const data: any = [];
-  let totalSeries: any = [];
+  const showOthers = true;
+  const data: Series[] = [];
+  let sellerSumSeries: any = [];
   const pieRef: any = useRef(null);
   const colors = [
     '#7cb5ec',
@@ -51,18 +68,59 @@ export default ({
     sellerInventories[key].data.forEach((dataPoint: any) => {
       const pointDate = dataPoint[0];
       const pointInventory = dataPoint[1];
-      if (pointDate in totalSeries) {
-        totalSeries[pointDate][1] += pointInventory;
+      if (pointDate in sellerSumSeries) {
+        sellerSumSeries[pointDate][1] += pointInventory;
       } else {
-        totalSeries[pointDate] = _.cloneDeep(dataPoint);
+        sellerSumSeries[pointDate] = _.cloneDeep(dataPoint);
       }
     });
   }
 
   // format `total` series data
-  totalSeries = Object.keys(totalSeries).map((key: any) => {
-    return totalSeries[key];
+  sellerSumSeries = Object.keys(sellerSumSeries).map((key: any) => {
+    return sellerSumSeries[key];
   });
+
+  // use an 'Others' series to reconcile discrepancies between product-level & sum of seller-level inventories
+  let otherSeries: Series | null | undefined = null;
+  if (showOthers) {
+    const otherSeriesData: [number, number][] = [];
+    let totalValue = 0;
+    const sellerSumSeriesDateValueMap: { [key: number]: number } = {};
+    sellerSumSeries.forEach((item: [number, number]) => {
+      sellerSumSeriesDateValueMap[item[0]] = item[1];
+    });
+
+    productInventories.forEach((item: [number, number]) => {
+      const pointDate = item[0];
+      const sellerSumInventory = sellerSumSeriesDateValueMap[pointDate]
+        ? sellerSumSeriesDateValueMap[pointDate]
+        : 0;
+      const productInventory = item[1];
+      const inventoryDiff = productInventory - sellerSumInventory;
+      if (inventoryDiff > 0) {
+        otherSeriesData.push([pointDate, inventoryDiff]);
+        totalValue += inventoryDiff;
+      }
+    });
+    otherSeriesData.sort((a, b) => {
+      if (a[0] > b[0]) return 1;
+      if (a[0] < b[0]) return -1;
+      return 0;
+    });
+
+    otherSeries = {
+      yAxis: 1,
+      type: 'column',
+      name: 'Other',
+      data: otherSeriesData,
+      totalValue: totalValue,
+      color: '#000000',
+    };
+
+    data.push(otherSeries);
+  }
+
   if (currentShowType === SHOW_TYPE.ProductLevelInventory) {
     data.push({
       yAxis: showRanks ? 1 : 0,
@@ -77,7 +135,7 @@ export default ({
       type: 'column',
       name: 'Inventory',
       color: '#4AD991',
-      data: totalSeries,
+      data: sellerSumSeries,
     });
   }
 
@@ -103,7 +161,7 @@ export default ({
   );
 
   // initialize yAxisOptions of time series chart
-  const inventoryDataPoints = totalSeries.map((item: any) => item[1]);
+  const inventoryDataPoints = sellerSumSeries.map((item: any) => item[1]);
   const inventoryYMin = Math.min(...inventoryDataPoints);
   const inventoryYMax = Math.max(...inventoryDataPoints, 0);
   const timeSeriesYAxisOptions: any = [
@@ -195,7 +253,7 @@ export default ({
             (item: any) =>
               item.name === 'Inventory' || (showRanks ? item.name === 'Rank' : undefined)
           )
-        : data.filter((item: any) => item.name !== 'Inventory'),
+        : data.filter((item: any) => item.name !== 'Inventory' || item.name !== 'Other'),
     plotOptions: {
       column: {
         stacking: 'normal',
@@ -260,6 +318,18 @@ export default ({
                     });
                   }
                 });
+              }
+
+              if (showOthers && otherSeries) {
+                const otherDataPoint = otherSeries.data.find(dataPoint => dataPoint[0] === x);
+                if (otherDataPoint) {
+                  newPieData.push({
+                    name: otherSeries.name,
+                    y: otherDataPoint[1],
+                    visible: true,
+                    color: otherSeries.color,
+                  });
+                }
               }
 
               setPieData(newPieData);
