bba185dc291944b2dfa512b5e3d04febde45d098
Richard Prasojo <48501717+rwprasojo@users.noreply.github.com>
2020-07-15 00:23:30 -0700

    Update RevenueChart.tsx
    remove //borderRadius: 3,

--- src/containers/Synthesis/Supplier/SupplierDetails/SupplierCharts/RevenueChart.tsx
+++ src/containers/Synthesis/Supplier/SupplierDetails/SupplierCharts/RevenueChart.tsx
@@ -122,7 +122,6 @@ class RevenueChart extends Component<RevenueChartProps> {
           pointPadding: 0.01,
           cropThreshold: 5,
           borderWidth: 0.1,
-          //borderRadius: 3,
           cursor: 'pointer',
         },
         series: {
