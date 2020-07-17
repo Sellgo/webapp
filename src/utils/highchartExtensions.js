43883016df40f941ad6d91bdab15b535d716e7eb
Marcus Ong <marcus.ong404@gmail.com>
2020-07-17 08:12:23 +0800

    fix reliability of centering donut title on resizing

--- src/utils/highchartExtensions.js
+++ src/utils/highchartExtensions.js
@@ -11,15 +11,19 @@ export const PercentAlign = function(H) {
     proceed.apply(this, [].slice.call(arguments, 1));
   });
 
-  H.wrap(H.Chart.prototype, 'reflow', function(proceed) {
-    // Run original proceed method
-    // eslint-disable-next-line prefer-rest-params
-    proceed.apply(this, [].slice.call(arguments, 1));
-
+  H.wrap(H.Chart.prototype, 'redraw', function(proceed) {
     const tOptions = this.options.title;
     if (tOptions && tOptions.percentAlign) {
       const percentAlign = tOptions.percentAlign;
       tOptions.x = ((this.chartWidth + this.plotLeft) * percentAlign) / 100;
+      console.log(this);
+      console.log(
+        `${this.chartWidth}|${this.plotLeft}|${tOptions.x}|||${this.plotSizeX}|${this.plotWidth}|${this.plotBox.x}`
+      );
     }
+
+    // Run original proceed method
+    // eslint-disable-next-line prefer-rest-params
+    proceed.apply(this, [].slice.call(arguments, 1));
   });
 };
