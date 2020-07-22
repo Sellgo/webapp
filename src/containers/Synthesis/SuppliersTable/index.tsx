a7f52e2c35362884162a67e1512bf5ccc860ef48
unknown <iamnaj999@gmail.com>
2020-07-23 05:30:27 +0800

    bugfr-344: Supplier Select Column removed search name, action and others action

--- src/containers/Synthesis/SuppliersTable/index.tsx
+++ src/containers/Synthesis/SuppliersTable/index.tsx
@@ -12,10 +12,8 @@ import {
 } from '../../../actions/Suppliers';
 import { currentSynthesisId } from '../../../selectors/UploadSupplier';
 import { connect } from 'react-redux';
-import { Dropdown, Icon, Confirm, Segment, Loader, Grid } from 'semantic-ui-react';
+import { Icon, Confirm, Segment, Loader, Grid } from 'semantic-ui-react';
 import { GenericTable, Column } from '../../../components/Table';
-import { Link } from 'react-router-dom';
-import history from '../../../history';
 import {
   suppliersSelector,
   suppliersTableTabSelector,
@@ -58,16 +56,6 @@ interface SuppliersTableProps {
 class SuppliersTable extends Component<SuppliersTableProps> {
   state = { showPieChartModalOpen: false, supplier: undefined, showDeleteConfirm: false };
 
-  renderName = (row: Supplier) => {
-    const name =
-      row.file_status === 'completed' ? (
-        <Link to={`/synthesis/${row.supplier_id}`}>{row.search}</Link>
-      ) : (
-        row.search
-      );
-    return <div className="supplier">{name}</div>;
-  };
-
   renderFileName = (row: Supplier) => {
     const { subscriptionType } = this.props;
     return (
@@ -83,75 +71,6 @@ class SuppliersTable extends Component<SuppliersTableProps> {
       </div>
     );
   };
-  renderActions = (row: Supplier) => {
-    const { amazonMWSAuthorized, handleUnauthorizedMwsAuth, subscriptionType } = this.props;
-    return (
-      <Dropdown
-        className={'syn-dropdown-link syn-dropdown-label'}
-        text="Profit Finder"
-        floating={true}
-        selectOnBlur={false}
-        fluid={true}
-        selection={true}
-        disabled={row.file_status !== 'completed' ? true : false}
-        options={[
-          {
-            key: '0',
-            text: <Dropdown.Item icon="spinner" text=" Profit Finder" />,
-            value: 'SYN',
-          },
-          {
-            key: '1',
-            text: isSubscriptionFree(subscriptionType) ? (
-              <Dropdown.Item icon="cart arrow down" text=" Download Search File" />
-            ) : (
-              <a href={row.file_url} download={true}>
-                <Dropdown.Item icon="cart arrow down" text=" Download Search File" />
-              </a>
-            ),
-            value: 'dwn_sp_file',
-            disabled: isSubscriptionFree(subscriptionType),
-          },
-          {
-            key: '2',
-            text:
-              row.report_url === null || isSubscriptionFree(subscriptionType) ? (
-                <Dropdown.Item icon="download" text=" Download Result File" />
-              ) : (
-                <a href={row.report_url} download={true}>
-                  <Dropdown.Item icon="download" text=" Download Result File" />
-                </a>
-              ),
-            value: 'dwn_res',
-            disabled:
-              row.report_url === null ? true : false || isSubscriptionFree(subscriptionType),
-          },
-          {
-            key: '3',
-            text: <Dropdown.Item icon="sync alternate" text=" Re-run" />,
-            value: 'rerun',
-            disabled: !amazonMWSAuthorized || isSubscriptionFree(subscriptionType),
-            onClick: e => {
-              if (isSubscriptionFree(subscriptionType)) {
-                e.stopPropagation();
-                return;
-              }
-              if (amazonMWSAuthorized) {
-                this.props.reRun(row);
-              } else {
-                handleUnauthorizedMwsAuth();
-              }
-            },
-          },
-        ]}
-        onChange={(e, data) => {
-          if (data.value === 'SYN') {
-            history.push(`/synthesis/${row.supplier_id}`);
-          }
-        }}
-      />
-    );
-  };
 
   handleCancelDelete = () => {
     this.setState({ showDeleteConfirm: false });
@@ -161,52 +80,6 @@ class SuppliersTable extends Component<SuppliersTableProps> {
     const { deleteSupplier } = this.props;
     const { supplier } = this.state;
     deleteSupplier(supplier);
-  };
-
-  renderOperations = (row: Supplier) => {
-    if (
-      row.file_status !== 'completed' &&
-      row.file_status !== 'inactive' &&
-      row.file_status !== 'failed' &&
-      row.file_status !== null &&
-      row.file_status !== undefined
-    ) {
-      return '';
-    }
-    const { favourite, unFavourite, subscriptionType } = this.props;
-    return (
-      <div className="operations">
-        <Icon
-          disabled={isSubscriptionFree(subscriptionType)}
-          name="thumbs up"
-          onClick={() => favourite(row.id, row.tag === 'like' ? '' : 'like')}
-          style={
-            !isSubscriptionFree(subscriptionType) && row.tag === 'like'
-              ? { color: '#349AF8' }
-              : { color: '#DEDEDF' }
-          }
-        />
-        <Icon
-          disabled={isSubscriptionFree(subscriptionType)}
-          name="thumbs down"
-          onClick={() => unFavourite(row.id, row.tag === 'dislike' ? '' : 'dislike')}
-          style={row.tag === 'dislike' ? { color: '#A2A2A2' } : { color: '#DEDEDF' }}
-        />
-        <Icon
-          disabled={isSubscriptionFree(subscriptionType)}
-          name="pencil"
-          style={{ color: '#DEDEDF' }}
-          onClick={() => this.props.onEdit(row)}
-        />
-        <Icon
-          disabled={isSubscriptionFree(subscriptionType)}
-          className={isSubscriptionFree(subscriptionType) ? `disabled` : ''}
-          name="trash alternate"
-          style={{ color: '#DEDEDF' }}
-          onClick={() => this.setState({ supplier: row, showDeleteConfirm: true })}
-        />
-      </div>
-    );
   };
 
   renderInventory = (row: Supplier) => {
@@ -259,14 +132,6 @@ class SuppliersTable extends Component<SuppliersTableProps> {
   };
 
   columns: Column[] = [
-    {
-      label: 'Search Name',
-      dataKey: 'search',
-      type: 'string',
-      sortable: true,
-      show: true,
-      render: this.renderName,
-    },
     {
       label: 'File Name',
       dataKey: 'file_name',
@@ -322,18 +187,6 @@ class SuppliersTable extends Component<SuppliersTableProps> {
       type: 'date',
       show: true,
       render: this.renderCompleted,
-    },
-    {
-      label: 'Action',
-      dataKey: 'action',
-      show: true,
-      render: this.renderActions,
-    },
-    {
-      label: 'Other',
-      dataKey: 'other',
-      show: true,
-      render: this.renderOperations,
     },
   ];
 
