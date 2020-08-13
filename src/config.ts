ecf4d61da3a6e7fe3e16096fe01cbd35c4bbf3d9
unknown <iamnaj999@gmail.com>
2020-08-14 02:44:35 +0800

    web url config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ const DEV_URLS = {
   BASE_URL_AUTH: 'sellgo-dev.auth0.com',
   BASE_URL_API: 'https://api.sellgo-dev.com/api/',
   CHANGE_PASS_API_URL: 'https://sellgo-dev.auth0.com/dbconnections/change_password',
+  WEB_URL: 'https://sellgo-dev.com',
 };
 
 const PROD_URLS = {
@@ -10,6 +11,7 @@ const PROD_URLS = {
   BASE_URL_AUTH: 'sellgo.auth0.com',
   BASE_URL_API: 'https://api.sellgo.com/api/',
   CHANGE_PASS_API_URL: 'https://sellgo.auth0.com/dbconnections/change_password',
+  WEB_URL: 'https://sellgo.com',
 };
 
 const DEV_KEYS = {
