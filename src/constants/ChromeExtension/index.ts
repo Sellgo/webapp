766fb12a5229591c3d0041f6909e19ed9bd61e99
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-01-14 21:08:22 +0530

    Reset back the ID to dev local chrome ID

--- src/constants/ChromeExtension/index.ts
+++ src/constants/ChromeExtension/index.ts
@@ -1,5 +1,7 @@
-const PROD_CHROME_ID = 'gldmigoakdolonchebfnmcfbjihelcec';
-const DEV_CHROME_ID = 'ckflgolkljenmgphflppbmbfdaoocdik';
+// place Chrome ID Here: changes for each person to test locally
+
+const PROD_CHROME_ID = 'eghepplelknleieeeiogjofmaneaadab';
+const DEV_CHROME_ID = 'eghepplelknleieeeiogjofmaneaadab';
 
 const chromeIDConfig = {
   PROD_CHROME_ID,
