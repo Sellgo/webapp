e66034c7dbbc330ae720467b81d5ff101f5c02fb
Pancham <dev.pancham.singh@gmail.com>
2021-01-16 02:59:21 +0530

    removing sandbox changes

--- src/config.ts
+++ src/config.ts
@@ -82,8 +82,7 @@ const local = {
   ...DEV_KEYS,
   ...DEV_ANALYTICS,
   ...DEV_MWS,
-  // callbackUrl: 'http://localhost:3000/callback',
-  callbackUrl: 'http://3.210.205.60:5001/callback',
+  callbackUrl: 'http://localhost:3000/callback',
 };
 
 function getAppConfig() {
