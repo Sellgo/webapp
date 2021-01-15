27f4bd1c77aca47a9a2579f42bba57ea1d257fdc
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-01-16 00:52:26 +0530

    Sandbox ready config files for chrome-ext-test

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const DEV_URLS = {
   BASE_URL: 'https://app.sellgo-dev.com',
   BASE_URL_AUTH: 'sellgo-dev.auth0.com',
-  BASE_URL_API: 'https://api.sellgo-dev.com/api/',
+  BASE_URL_API: 'http://34.236.237.125/api/',
   CHANGE_PASS_API_URL: 'https://sellgo-dev.auth0.com/dbconnections/change_password',
   WEB_URL: 'https://sellgo-dev.com',
 };
@@ -80,7 +80,7 @@ const local = {
   ...DEV_KEYS,
   ...DEV_ANALYTICS,
   ...DEV_MWS,
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://3.210.205.60:5000/callback',
 };
 
 function getAppConfig() {
---
+++ src/constants/ChromeExtension/index.ts
@@ -0,0 +1,12 @@
+// place Chrome ID Here: changes for each person to test locally
+
+// pointed to Richard's local ID rigt now...
+const PROD_CHROME_ID = 'ckflgolkljenmgphflppbmbfdaoocdik';
+const DEV_CHROME_ID = 'ckflgolkljenmgphflppbmbfdaoocdik';
+
+const chromeIDConfig = {
+  PROD_CHROME_ID,
+  DEV_CHROME_ID,
+};
+
+export default chromeIDConfig;
