cb36b0c33efe3ddf9c37d50904d2fbdfddba5db6
vdarmadi <victor@sellgo.com>
2021-01-19 10:15:51 -0800

    ENG-1131 Finalize configs

--- src/constants/ChromeExtension/index.ts
+++ src/constants/ChromeExtension/index.ts
@@ -1,8 +1,6 @@
-// place Chrome ID Here: changes for each person to test locally
-
-// pointed to Richard's local ID rigt now...
-const PROD_CHROME_ID = 'ckflgolkljenmgphflppbmbfdaoocdik';
-const DEV_CHROME_ID = 'ckflgolkljenmgphflppbmbfdaoocdik';
+// place Chrome ID Here: change for each person to test locally
+const PROD_CHROME_ID = 'gldmigoakdolonchebfnmcfbjihelcec';
+const DEV_CHROME_ID = 'gldmigoakdolonchebfnmcfbjihelcec';
 
 const chromeIDConfig = {
   PROD_CHROME_ID,
