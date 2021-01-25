98256bafb12601dea0885db1459b1675e956ec5a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-01-26 03:51:16 +0530

    Adding dependencies on use Effect

--- src/components/Chart/ProfitFinderChart.tsx
+++ src/components/Chart/ProfitFinderChart.tsx
@@ -22,9 +22,24 @@ const ProfitFinderChart = (props: any) => {
           : null;
       chartComponentRef.current.chart.setSize(undefined, chartHeight);
     }
+  }, []);
+
+  useEffect(() => {
+    if (chartComponentRef && chartComponentRef.current && chartComponentRef.current.chart) {
+      const chartHeight =
+        windowSize.width && windowSize.width >= 2560
+          ? 483
+          : windowSize.width && windowSize.width >= 1920
+          ? 363
+          : windowSize.width && windowSize.width >= 1368
+          ? 260
+          : null;
+      chartComponentRef.current.chart.setSize(undefined, chartHeight);
+    }
   }, [windowSize]);
 
   const nextProps = { ...props, profitFinderChartOptions, chartComponentRef };
+  console.log('Triggered');
   return props.render(nextProps);
 };
 
