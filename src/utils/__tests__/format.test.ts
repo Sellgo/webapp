b7374923d72df994cd88819fd581607e11e16f5a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-03-02 21:21:40 +0530

    Testing format rating util

--- src/utils/__tests__/format.test.ts
+++ src/utils/__tests__/format.test.ts
@@ -1,4 +1,4 @@
-import { formatCurrency, formatNumber } from '../format';
+import { formatCurrency, formatNumber, formatRating } from '../format';
 
 /* Testing formatCurrency Utility */
 describe('Testing format currency utility', () => {
@@ -37,5 +37,27 @@ describe('Testing format number utility', () => {
     expect(formatNumber(null)).toEqual('0');
     expect(formatNumber(undefined)).toEqual('NaN');
     expect(formatNumber('1,000')).toEqual('NaN');
+  });
+});
+
+/* Testing formatRating Utility */
+describe('Testing format rating utility', () => {
+  test('Testing numeric values', () => {
+    expect(formatRating(234)).toEqual('234.0');
+    expect(formatRating(1000.2345)).toEqual('1000.2');
+    expect(formatRating(0)).toEqual('0.0');
+    expect(formatRating(0.67)).toEqual('0.7');
+  });
+
+  test('Testing string values', () => {
+    expect(formatRating('1234')).toEqual('1234.0');
+    expect(formatRating('1234.5')).toEqual('1234.5');
+    expect(formatRating('1234.77')).toEqual('1234.8');
+    expect(formatRating('1,234.5')).toEqual('NaN');
+  });
+
+  test('Testing null and undefined values', () => {
+    expect(formatRating(null)).toEqual('0.0');
+    expect(formatRating(undefined)).toEqual('NaN');
   });
 });
