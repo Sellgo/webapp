3635917a7ff58741205c9309b2a539fa187c9e3d
Richard <richard@sellgo.com>
2021-04-02 12:46:06 -0700

    changes from Richard on new design look

--- src/components/WithoutCostUpload/index.tsx
+++ src/components/WithoutCostUpload/index.tsx
@@ -1,7 +1,5 @@
-import no_cost_icon from '../../assets/images/no-cost.svg';
-import React from 'react';
 import './index.scss';
 
 export const WithoutCostUpload = () => {
-  return <img src={no_cost_icon} alt={'icon'} style={{ float: 'right', marginLeft: 3 }} />;
+  return null;
 };
