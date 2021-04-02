ec6b54f6626b3b65aedeb89596b657669ec57883
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-04-02 19:36:05 +0530

    Adding spacing for the react deop down file uploader and center things

--- src/components/FileUploader/index.tsx
+++ src/components/FileUploader/index.tsx
@@ -10,6 +10,7 @@ interface FileUploaderProps {
   accept: string[];
   fileDetails: any;
 }
+
 function FileUploader(props: FileUploaderProps) {
   const { accept, onDrop, onDropRejected, fileDetails } = props;
   const multiple = false;
@@ -30,16 +31,15 @@ function FileUploader(props: FileUploaderProps) {
       <br />
       <p className={`description-container ${styles.description} ${styles['margin-top']}`}>
         <b className="upload-supplier-title">Upload filled-in Supplier File(s) here</b>
-        <br />
+
         <span className={`upload-supplier-desc ${styles.description}`}>
           Drag and drop, or click to select
         </span>
-        <br />
+
         <span className={`upload-supplier-desc ${styles.description}`}>
           Accepted File Types: {accept.join(', ')}
         </span>
-        <br />
-        <br />
+
         <p className={styles.filename}>{fileDetails !== null ? fileDetails.name : ' '}</p>
       </p>
     </button>
