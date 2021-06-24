a5fa770ac7e23135c9ba2d29352e560dcab0b4ad
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-06-24 20:02:03 +0530

    Addition of the zoom controls on the map

--- src/containers/MapTest/index.tsx
+++ src/containers/MapTest/index.tsx
@@ -7,12 +7,30 @@ import { MapContainer, TileLayer, Marker, Circle, Popup, useMap } from 'react-le
 import styles from './index.module.scss';
 
 type Location = [number, number];
-const center: Location = [37.09024, -95.712891];
-const zoom = 5;
+
+const INITIAL_CENTER: Location = [37.09024, -95.712891];
+const INITIAL_ZOOM = 4.8;
+const MIN_ZOOM = 2.5;
+const MAX_ZOOM = 8.7;
+const WORLD_MAP_BOUNDS: Location[] = [
+  [-90, -180],
+  [90, 180],
+];
+
+const SellerDetailsCard = () => {
+  return (
+    <>
+      <article className={styles.sellerCard}>
+        <h1>Seller Name</h1>
+        <p>Sales Estimate</p>
+      </article>
+    </>
+  );
+};
 
 const UseHooksComponent = () => {
   const map = useMap();
-  console.log('map center:', map.getCenter());
+  console.log('Map is', map);
 
   // return null on hooks
   return null;
@@ -22,17 +40,29 @@ const UseHooksComponent = () => {
 const PlotAllMarkers = (props: any) => {
   const { sellersData } = props;
 
+  const [showSellerCard, setShowSellerCard] = useState(false);
+
   return (
     <>
       {sellersData.map((data: any) => {
         const center: Location = [data.latitude, data.longitude];
         return (
-          <Marker position={center} key={data.merchant_id}>
+          <Marker
+            position={center}
+            key={data.merchant_id}
+            eventHandlers={{
+              click: e => {
+                console.log('marker clicked', e);
+                setShowSellerCard(true);
+              },
+            }}
+          >
             <Circle center={center} radius={10000} fillColor="red" fillOpacity={0.8} />
             <Popup>Clicked merchant {data.merchant_id}</Popup>
           </Marker>
         );
       })}
+      {showSellerCard && <SellerDetailsCard />}
     </>
   );
 };
@@ -52,16 +82,19 @@ const MapTest = () => {
     <section className={styles.mapContainer}>
       <MapContainer
         animate
-        center={center}
-        zoom={zoom}
-        minZoom={2}
-        maxZoom={10}
+        center={INITIAL_CENTER}
+        zoom={INITIAL_ZOOM}
+        minZoom={MIN_ZOOM}
+        maxZoom={MAX_ZOOM}
         closePopupOnClick
         className={styles.map}
+        doubleClickZoom={false}
+        maxBounds={WORLD_MAP_BOUNDS}
       >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap
         />
         <UseHooksComponent />
         <PlotAllMarkers sellersData={mapData} />
