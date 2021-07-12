50f6738b70517490235d4c7b098133cb1433a117
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-13 02:19:11 +0530

    BUGFR-1636: Increase max zoom level to 18

--- src/constants/SellerMap/index.ts
+++ src/constants/SellerMap/index.ts
@@ -13,7 +13,7 @@ export const actionTypes = {
 export const INITIAL_CENTER: Location = [37.09024, -95.712891];
 export const INITIAL_ZOOM = 4.8;
 export const MIN_ZOOM = 2.5;
-export const MAX_ZOOM = 10;
+export const MAX_ZOOM = 18;
 export const WORLD_MAP_BOUNDS: Location[] = [
   [-90, -180],
   [90, 180],
