3aaadcfb0d230d157316fc855ba8c912b17d68ad
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-14 00:45:56 +0530

    Addition of section for advanced filter section and toggle

---
+++ src/containers/ProductResearch/ProductsPanel/AdvancedFilters/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* Styling */
+import styles from './index.module.scss';
+
+const AdvancedFilters = () => {
+  return (
+    <div className={styles.advancedFiltersOptions}>
+      <p>Adnacbed Filter Options</p>
+    </div>
+  );
+};
+
+export default AdvancedFilters;
--- src/containers/ProductResearch/ProductsPanel/index.tsx
+++ src/containers/ProductResearch/ProductsPanel/index.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Icon } from 'semantic-ui-react';
 
 /* Components */
 import BasicFilters from './BasicFilters';
+import AdvancedFilters from './AdvancedFilters';
 
 /* Styling */
 import styles from './index.module.scss';
 
 const ProductPanel = () => {
+  const [showAdvancedFilter, setShowAdvancedFilter] = useState<boolean>(false);
+
   return (
     <>
       <section className={styles.filterSection}>
-        <BasicFilters />
-        <div className={styles.advancedFilter}>
-          <p>Advanced Filter will go here</p>
+        {/* Basic Filter */}
+        <div className={styles.showbasicFilters}>
+          <BasicFilters />
+        </div>
+
+        {/* Advanced Filter */}
+        <div className={styles.advancedFilterWrapper}>
+          <div
+            className={styles.advancedFilterToggle}
+            onClick={() => setShowAdvancedFilter(prevState => !prevState)}
+          >
+            <span>Advanced Filters</span>
+            <span>
+              {showAdvancedFilter ? <Icon name="chevron up" /> : <Icon name="chevron down" />}
+            </span>
+          </div>
+
+          {showAdvancedFilter && (
+            <div className={styles.showAdvancedFilter}>
+              <AdvancedFilters />
+            </div>
+          )}
         </div>
       </section>
 
