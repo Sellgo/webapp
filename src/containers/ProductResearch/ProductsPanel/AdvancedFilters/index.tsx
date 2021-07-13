65e551af9a9be50e01e62aa64582b63a37b72799
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-14 01:41:46 +0530

    Replace flexbox with grid for advanced filter, keep flexbox code for now

--- src/containers/ProductResearch/ProductsPanel/AdvancedFilters/index.tsx
+++ src/containers/ProductResearch/ProductsPanel/AdvancedFilters/index.tsx
@@ -7,6 +7,7 @@ import styles from './index.module.scss';
 import MinMaxFilter from '../../../../components/FormFilters/MinMaxFilter';
 import SelectionFilter from '../../../../components/FormFilters/SelectionFilter';
 import InputFilter from '../../../../components/FormFilters/InputFilter';
+import CheckboxListFilter from '../../../../components/FormFilters/CheckboxListFilter';
 
 const options = [
   {
@@ -24,19 +25,23 @@ const options = [
 const AdvancedFilters = () => {
   return (
     <div className={styles.advancedFiltersOptions}>
+      {/* First Row */}
       <MinMaxFilter label="Sales Year Over Year (%)" />
       <MinMaxFilter label="Price Change (%)" />
       <MinMaxFilter label="Sales Change (%)" />
-      <MinMaxFilter label="Sales Year Over Year (%)" />
       <SelectionFilter filterOptions={options} label="Best Sales Period" placeholder="Month" />
       <MinMaxFilter label="Sales to Reviews" />
       <MinMaxFilter label="Monthly Sales (units)" />
+
+      {/* Second Row */}
       <MinMaxFilter label="BSR" />
       <MinMaxFilter label="# of Sellers" />
       <MinMaxFilter label="# of Images" />
+      <CheckboxListFilter label="Fulfillment" />
       <MinMaxFilter label="Variation Count" />
       <MinMaxFilter label="Weight(lbs)" />
-      <MinMaxFilter label="Weight(lbs)" />
+
+      {/* last Row*/}
       <InputFilter label="Include Title Keywords" placeholder="Enter keywords" />
       <InputFilter label="Exclude Title Keywords" placeholder="Enter keywords" />
       <InputFilter label="Include Brands" placeholder="Enter brands" />
