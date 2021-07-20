df1ca2bb9fc86de16082454267c486a662b10740
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-20 23:48:21 +0530

    Clear console logs

--- src/containers/Settings/Subscription/index.tsx
+++ src/containers/Settings/Subscription/index.tsx
@@ -189,8 +189,6 @@ class SubscriptionPricing extends React.Component<SubscriptionProps> {
 
   getReferral() {
     // @ts-ignore
-    console.log('Getting Rewardful Data', window.Rewardful.referral);
-    // @ts-ignore
     return typeof window.Rewardful !== 'undefined' && window.Rewardful.referral;
   }
 
