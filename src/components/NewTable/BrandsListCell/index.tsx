7ff961ca9424f6f4794c079dc5f0aca478d63c5b
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-06 19:44:23 +0530

    BUGFR-1736: Add more copy function

--- src/components/NewTable/BrandsListCell/index.tsx
+++ src/components/NewTable/BrandsListCell/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Table } from 'rsuite';
+import { Button, Icon, Popup } from 'semantic-ui-react';
+
+/* Styles */
+import styles from './index.module.scss';
 
 /* Utils */
 import { removeSpecialChars, showNAIfZeroOrNull } from '../../../utils/format';
+import { copyToClipboard } from '../../../utils/file';
+import { success } from '../../../utils/notifications';
 
 /* Types */
 import { RowCell } from '../../../interfaces/Table';
+
+/* Components */
 import CopyToClipboard from '../../CopyToClipboard';
 
 /* Row cell, Appends $ sign infront of monetary cells */
@@ -16,14 +24,55 @@ const BrandsListCell = (props: RowCell) => {
 
   const parsedBrands = JSON.parse(JSON.stringify(brands));
 
+  /* Copy Asins */
+  const handleCopyBrands = (deliminator?: string) => {
+    const prepareAsinStringCopy = removeSpecialChars(parsedBrands, deliminator);
+    copyToClipboard(prepareAsinStringCopy).then(() => {
+      success('ASINs successfully copied');
+    });
+  };
+
   return (
     <Table.Cell {...props}>
-      <div>
+      <div className={styles.brandsListWrapper}>
         {parsedBrands.length > 0 ? (
-          <CopyToClipboard
-            displayData={showNAIfZeroOrNull(parsedBrands.length, parsedBrands.length)}
-            data={removeSpecialChars(parsedBrands)}
-          />
+          <>
+            <CopyToClipboard
+              displayData={showNAIfZeroOrNull(parsedBrands.length, parsedBrands.length)}
+              data={removeSpecialChars(parsedBrands)}
+            />
+            <Popup
+              on="click"
+              position="bottom left"
+              offset="-40"
+              closeOnDocumentClick
+              closeOnEscape
+              className={styles.actionsPopover}
+              content={
+                <>
+                  <div className={styles.actionOptions}>
+                    <p>BRANDS</p>
+
+                    <button onClick={() => handleCopyBrands(',')}>
+                      <Icon name="copy outline" />
+                      <span>Copy ASINs in rows with comma</span>
+                    </button>
+
+                    <button onClick={() => handleCopyBrands('\n')}>
+                      <Icon name="copy outline" />
+                      <span>Copy ASINs in columns</span>
+                    </button>
+                  </div>
+                </>
+              }
+              trigger={
+                <Button
+                  icon="chevron down"
+                  className={`${styles.iconButton} iconButtonResetGlobal`}
+                />
+              }
+            />
+          </>
         ) : (
           <span>-</span>
         )}
