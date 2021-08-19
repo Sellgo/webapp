ddf0a0c35ed64dcba3cfabb767a289b7bbf0464e
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-19 23:27:43 +0530

    Corretcion of max asin limit on keyword research

--- src/containers/KeywordResearch/KeywordReverse/ReverseAsinsList/index.tsx
+++ src/containers/KeywordResearch/KeywordReverse/ReverseAsinsList/index.tsx
@@ -83,7 +83,7 @@ const ReverseAsinsList = (props: Props) => {
           placeholder="Enter asins spearted by comma"
           value={toAddAsins.toUpperCase()}
           onChange={handleAddNewAsins}
-          disabled={asinList.length === 10}
+          disabled={asinList.length === MAX_ASINS_ALLOWED}
         />
         <span className={styles.infoDetail}>
           {MAX_ASINS_ALLOWED - asinList.length || MAX_ASINS_ALLOWED} more ASINs allowed.
