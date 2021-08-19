d3a5071df4b98f0d869a080568f7c13ac0e4fe04
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-19 22:14:50 +0530

    Addition of the export container

---
+++ src/containers/KeywordResearch/KeywordDatabase/DatabaseExport/index.tsx
@@ -0,0 +1,25 @@
+import React, { useMemo } from 'react';
+
+/* Styling */
+import styles from './index.module.scss';
+
+/* Utils */
+import { formatNumber } from '../../../../utils/format';
+
+const DatabaseExport = () => {
+  /*Total Products */
+  const totalProductsFound = useMemo(() => {
+    const count = 10_0000;
+    return formatNumber(count);
+  }, []);
+
+  return (
+    <div className={styles.exportsContainer}>
+      <p className={styles.messageText}>
+        Viewing <span className={styles.sellerCount}>{totalProductsFound}</span> products.
+      </p>
+    </div>
+  );
+};
+
+export default DatabaseExport;
--- src/containers/KeywordResearch/KeywordDatabase/index.tsx
+++ src/containers/KeywordResearch/KeywordDatabase/index.tsx
@@ -6,11 +6,13 @@ import styles from './index.module.scss';
 /* Containers */
 import DatabaseFilters from './DatabaseFilters';
 import DatabaseTable from './DatabaseTable';
+import DatabaseExport from './DatabaseExport';
 
 const KeywordDatabase = () => {
   return (
     <main className={styles.keywordDatabasePage}>
       <DatabaseFilters />
+      <DatabaseExport />
       <DatabaseTable />
     </main>
   );
