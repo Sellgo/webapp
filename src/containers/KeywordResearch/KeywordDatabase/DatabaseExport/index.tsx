6bc8473a24250088572e3f7c46eba0837e93da41
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-23 22:22:07 +0530

    Stitch export on the keyword database export

--- src/containers/KeywordResearch/KeywordDatabase/DatabaseExport/index.tsx
+++ src/containers/KeywordResearch/KeywordDatabase/DatabaseExport/index.tsx
@@ -1,25 +1,91 @@
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Icon } from 'semantic-ui-react';
 
 /* Styling */
 import styles from './index.module.scss';
 
 /* Utils */
-import { formatNumber } from '../../../../utils/format';
+import { downloadFile } from '../../../../utils/download';
+import { success } from '../../../../utils/notifications';
 
-const DatabaseExport = () => {
-  /*Total Products */
-  const totalProductsFound = useMemo(() => {
-    const count = 10_0000;
-    return formatNumber(count);
-  }, []);
+/* Selectors */
+import {
+  getkeywordDatabaseProgressData,
+  getkeywordDatabaseTablePaginationInfo,
+} from '../../../../selectors/KeywordResearch/KeywordDatabase';
+
+/* Interface */
+import {
+  KeywordDatabaseProgressData,
+  KeywordDatabasePaginationInfo,
+} from '../../../../interfaces/KeywordResearch/KeywordDatabase';
+
+/* Constants */
+import { EXPORT_DATA, EXPORT_FORMATS } from '../../../../constants/KeywordResearch/KeywordDatabase';
+
+/* Components */
+import ExportResultAs from '../../../../components/ExportResultAs';
+
+interface Props {
+  keywordDatabaseProgressData: KeywordDatabaseProgressData;
+  keywordDatabasePaginationInfo: KeywordDatabasePaginationInfo;
+}
+
+const DatabaseExport = (props: Props) => {
+  const { keywordDatabaseProgressData, keywordDatabasePaginationInfo } = props;
+
+  const [openExports, setOpenExports] = useState(false);
+
+  const handleOnExport = async () => {
+    if (keywordDatabaseProgressData.report_xlsx_url) {
+      await downloadFile(keywordDatabaseProgressData.report_xlsx_url);
+      success('File successfully downloaded');
+      setOpenExports(false);
+    }
+  };
+
+  const shouldEnableExport = keywordDatabaseProgressData.report_xlsx_url;
 
   return (
-    <div className={styles.exportsContainer}>
-      <p className={styles.messageText}>
-        Viewing <span className={styles.sellerCount}>{totalProductsFound}</span> products.
-      </p>
-    </div>
+    <>
+      <section className={styles.exportsContainer}>
+        {keywordDatabasePaginationInfo.total_pages > 0 && (
+          <p className={styles.messageText}>
+            Viewing{' '}
+            <span className={styles.sellerCount}>{keywordDatabasePaginationInfo.count}</span>{' '}
+            products.
+          </p>
+        )}
+        <div
+          onClick={() => (shouldEnableExport ? setOpenExports(true) : 0)}
+          className={`${styles.exportButton} ${
+            shouldEnableExport ? 'export-button' : 'export-button-disabled'
+          }`}
+        >
+          <Icon name="download" />
+          <span>Export</span>
+        </div>
+      </section>
+
+      <ExportResultAs
+        open={openExports}
+        formats={EXPORT_FORMATS}
+        data={EXPORT_DATA}
+        onClose={() => setOpenExports(false)}
+        loading={false}
+        onExport={handleOnExport}
+        format={'xlsx'}
+      />
+    </>
   );
 };
 
-export default DatabaseExport;
+const mapStateToProps = (state: any) => {
+  return {
+    keywordDatabaseProgressData: getkeywordDatabaseProgressData(state),
+    keywordDatabasePaginationInfo: getkeywordDatabaseTablePaginationInfo(state),
+  };
+};
+
+export default connect(mapStateToProps)(DatabaseExport);
