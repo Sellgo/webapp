7b6059ec211be82374b067df7318caf3ee1d22c5
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-30 23:52:35 +0530

    Hide lengedn in highcharts

--- src/containers/KeywordResearch/KeywordTracker/TrackerKeywordTable/KeywordHistoryChart/index.tsx
+++ src/containers/KeywordResearch/KeywordTracker/TrackerKeywordTable/KeywordHistoryChart/index.tsx
@@ -65,7 +65,7 @@ export const KeywordHistoryChart = (props: Props) => {
       stickOnContact: true,
     },
     legend: {
-      enaabled: false,
+      enabled: false,
     },
     plotOptions: {
       series: {
