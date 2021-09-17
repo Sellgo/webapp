c58383c6d953a43ce515932d4f51566ef5bdf18f
pongzers <you@example.com>
2021-09-18 05:50:54 +0800

    Update nav bar, update seller research color, add onboarding for seller research

---
+++ src/constants/SellerResearch/SellerResearch.ts
@@ -0,0 +1,4 @@
+export const SELLER_RESEARCH_FEATURES = [
+  { index: 0, name: 'Seller Database' },
+  { index: 1, name: 'Seller Map' },
+];
--- src/containers/SellerResearch/index.tsx
+++ src/containers/SellerResearch/index.tsx
@@ -7,8 +7,6 @@ import styles from './index.module.scss';
 
 /* Components */
 import PageHeader from '../../components/PageHeader';
-import QuotaMeter from '../../components/QuotaMeter';
-import MarketplaceDropdown from '../../components/MarketplaceDropdown';
 
 /* Containers */
 import SellerMaps from './SellerMaps';
@@ -17,19 +15,32 @@ import SellerDatabase from './SellerDatabase';
 /* Actions */
 import { setUserOnboardingResources } from '../../actions/UserOnboarding';
 
+/* Selectors */
+import { getUserOnboarding, getUserOnboardingResources } from '../../selectors/UserOnboarding';
+
 /* Assets */
 import sellerDatabaseOnborading from '../../assets/onboardingResources/SellerResearch/sellerDatabaseOnboarding.json';
 import sellerMapOnborading from '../../assets/onboardingResources/SellerResearch/sellerMapOnboarding.json';
 
+/* Constants */
+import { SELLER_RESEARCH_FEATURES } from '../../constants/SellerResearch/SellerResearch';
+import OnboardingButton from '../../components/OnboardingButton';
+import {
+  FALLBACK_ONBOARDING_DETAILS,
+  GENERAL_TUTORIAL_INDEX,
+} from '../../constants/UserOnboarding';
+
 interface Props {
   match: any;
   setUserOnboardingResources: (payload: any) => void;
+  userOnboarding: boolean;
+  userOnboardingResources: any[];
 }
 
 const SellerResearchMapper = ['Database', 'Map', 'Inventories'];
 
 const SellerResearch = (props: Props) => {
-  const { match, setUserOnboardingResources } = props;
+  const { match, setUserOnboardingResources, userOnboardingResources, userOnboarding } = props;
 
   const [selectedTabList, setSelectedTabList] = useState<number>(0);
 
@@ -45,6 +56,13 @@ const SellerResearch = (props: Props) => {
     }
   }, [selectedTabList]);
 
+  /* User onboarding logic */
+  const tutorialOnboardingDetails = userOnboardingResources[GENERAL_TUTORIAL_INDEX] || {};
+  const showTutorialOnboarding =
+    userOnboarding && Object.keys(tutorialOnboardingDetails).length > 0;
+  const { youtubeLink, displayText } =
+    tutorialOnboardingDetails.Tutorial || FALLBACK_ONBOARDING_DETAILS;
+
   return (
     <>
       <PageHeader
@@ -52,8 +70,8 @@ const SellerResearch = (props: Props) => {
         breadcrumb={[
           { content: 'Home', to: '/' },
           { content: 'Seller Research', to: '/seller-research' },
+          { content: SELLER_RESEARCH_FEATURES[selectedTabList].name, to: '/seller-research' },
         ]}
-        callToAction={<QuotaMeter />}
         auth={match.params.auth}
       />
 
@@ -61,7 +79,9 @@ const SellerResearch = (props: Props) => {
         {/* Filter meta data */}
         <section className={styles.filterMetaData}>
           <h1>Seller Research: {SellerResearchMapper[selectedTabList]}</h1>
-          <MarketplaceDropdown />
+          {showTutorialOnboarding && (
+            <OnboardingButton displayMessage={displayText} youtubeLink={youtubeLink} />
+          )}
         </section>
 
         {/* Filter product selection */}
@@ -73,8 +93,8 @@ const SellerResearch = (props: Props) => {
             selectedIndex={selectedTabList}
           >
             <TabList className={styles.productTablist}>
-              <Tab>Sellers</Tab>
-              <Tab>Map</Tab>
+              <Tab>DATABASE</Tab>
+              <Tab>MAP</Tab>
             </TabList>
 
             <TabPanel>
@@ -97,4 +117,11 @@ const mapDispatchToProps = (dispatch: any) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(SellerResearch);
+const mapStateToProps = (state: any) => {
+  return {
+    userOnboarding: getUserOnboarding(state),
+    userOnboardingResources: getUserOnboardingResources(state),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SellerResearch);
