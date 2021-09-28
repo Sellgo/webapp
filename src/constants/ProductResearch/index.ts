38e7e94a2b4f9bb384148c474d9ad315651b328d
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-09-28 16:38:02 +0530

    Add new components to product research

---
+++ src/constants/ProductResearch/index.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_RESEARCH_PRODUCT_DETAILS = [
+  {
+    name: 'Product Database',
+    desc: 'Easily Identify Amazon Products by Category, Brand, and much more.',
+  },
+];
--- src/constants/SellerResearch/index.ts
+++ src/constants/SellerResearch/index.ts
@@ -1,8 +1,7 @@
 export const SELLER_RESEARCH_PRODUCT_DETAILS = [
   {
-    index: 0,
     name: 'Seller Database',
     desc: 'Easily Identify Amazon Sellers by Category, Brand, and much more.',
   },
-  { index: 1, name: 'Seller Map', desc: 'Locate and Scout Millions of Amazon Sellers in seconds.' },
+  { name: 'Seller Map', desc: 'Locate and Scout Millions of Amazon Sellers in seconds.' },
 ];
