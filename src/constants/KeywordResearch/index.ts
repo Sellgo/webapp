8926c929c07cec2fdc80b28b4f26125eb2609d9d
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-09-28 16:52:24 +0530

    Add new componenents for keyword research

---
+++ src/constants/KeywordResearch/index.ts
@@ -0,0 +1,14 @@
+export const KEYWORD_RESEARCH_PRODUCT_DETAILS = [
+  {
+    name: 'Keyword Reverse',
+    desc: 'Add info for Reverse',
+  },
+  {
+    name: 'Keyword Database',
+    desc: 'Add info database',
+  },
+  {
+    name: 'Keyword Tracker',
+    desc: 'Add info for tracker',
+  },
+];
