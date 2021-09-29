412bdb390a649469595a4a7412fb4110a17dc20a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-09-29 20:17:04 +0530

    Add bulk ASIn workflow WIP

--- src/components/AddReverseBulkAsins/index.tsx
+++ src/components/AddReverseBulkAsins/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /* Styling */
 import styles from './index.module.scss';
 
+/* Components */
+import TextAreaInput from '../FormFilters/TextAreaInput';
+
 const AddReverseBulkAsins = () => {
+  const [reverseAsins, setReverseAsins] = useState<string>('');
   return (
     <div className={styles.addReverseBulkAsinsModal}>
-      <div className={styles.metaData}>
-        <h1>ADD NEW KEYWORDS</h1>
+      <div className={styles.filterForm}>
+        <TextAreaInput
+          label="Add Bulk ASINs"
+          placeholder="Enter ASINs (1 per line..)"
+          value={reverseAsins}
+          handleChange={value => {
+            setReverseAsins(value);
+          }}
+        />
       </div>
     </div>
   );
