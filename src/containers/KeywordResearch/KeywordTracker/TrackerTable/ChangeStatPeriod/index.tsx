11f6d70934cfbc80669decb231c7ac6b129127e2
pongzers <you@example.com>
2021-11-16 07:02:45 +0800

    Halfway for tooltip

--- src/containers/KeywordResearch/KeywordTracker/TrackerTable/ChangeStatPeriod/index.tsx
+++ src/containers/KeywordResearch/KeywordTracker/TrackerTable/ChangeStatPeriod/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Table } from 'rsuite';
+import { connect } from 'react-redux';
+import { Popup } from 'semantic-ui-react';
 
 /* Styling */
 import styles from './index.module.scss';
+
+/* Component */
+// import OnboardingTooltip from '../../../../../components/OnboardingTooltip';
+import TooltipMessage from '../../../../../components/TooltipMessage';
+
+/* Constants */
+import {
+  FALLBACK_ONBOARDING_DETAILS,
+  TABLE_KPI_ONBOARDING_INDEX,
+} from '../../../../../constants/UserOnboarding';
+
+/* Selectors */
+import {
+  getUserOnboarding,
+  getUserOnboardingResources,
+} from '../../../../../selectors/UserOnboarding';
 
 /* Assets */
 import { ReactComponent as Top10Icon } from '../../../../../assets/images/top10.svg';
@@ -11,15 +29,43 @@ import { ReactComponent as Top50Icon } from '../../../../../assets/images/top50.
 /* Interfaces */
 import { RowCell } from '../../../../../interfaces/Table';
 
-const ChangeStatPeriod = (props: RowCell) => {
+interface Props extends RowCell {
+  userOnboarding: boolean;
+  userOnboardingResources: any[];
+}
+const ChangeStatPeriod = (props: Props) => {
+  const { userOnboardingResources, ...otherProps } = props;
+  const { dataKey } = otherProps;
+
+  const tableKpiOnboardingDetails = userOnboardingResources[TABLE_KPI_ONBOARDING_INDEX] || {};
+  // const showTableKpiOnboarding =
+  //   userOnboarding && Object.keys(tableKpiOnboardingDetails).length > 0;
+  const { tooltipText } = tableKpiOnboardingDetails[dataKey] || FALLBACK_ONBOARDING_DETAILS;
+
   return (
-    <Table.Cell {...props}>
+    <Table.Cell {...otherProps}>
       <div className={styles.changeStatPeriod}>
-        <Top10Icon className={styles.top50Icon} />
+        <Popup
+          on="hover"
+          className={
+            tooltipText && tooltipText.length > 0
+              ? styles.onboardingTooltipPopup
+              : styles.hideTooltipPopup
+          }
+          trigger={<Top10Icon className={styles.top50Icon} />}
+          content={tooltipText && <TooltipMessage tooltipMessage={tooltipText} />}
+        />
         <Top50Icon className={styles.top50Icon} />
       </div>
     </Table.Cell>
   );
 };
 
-export default ChangeStatPeriod;
+const mapStateToProps = (state: any) => {
+  return {
+    userOnboarding: getUserOnboarding(state),
+    userOnboardingResources: getUserOnboardingResources(state),
+  };
+};
+
+export default connect(mapStateToProps)(ChangeStatPeriod);
