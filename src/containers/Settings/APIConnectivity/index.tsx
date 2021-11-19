14bc3b0c9eb851d8aae83542527d5235b28f5cb1
pongzers <you@example.com>
2021-11-20 06:45:05 +0800

    add activation link in zapier integration

--- src/containers/Settings/APIConnectivity/index.tsx
+++ src/containers/Settings/APIConnectivity/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 import PageHeader from '../../../components/PageHeader';
 import styles from './index.module.scss';
 import APIForm from './APIForm';
@@ -31,10 +32,26 @@ const APIConnectivity = (props: Props) => {
           auth={match.params.auth}
         />
       )}
-      <main className={styles.connectivityPageWrapper}>
+      <div className={styles.connectivityPageWrapper}>
         <SettingsNav match={match} history={history} />
-        <APIForm />
-      </main>
+        <div>
+          <APIForm />
+          <p className={styles.zapierActivationLink}>
+            Since our app is still private Beta, please click on the link below to get invitation
+            access from Zapier:
+            <br />
+            <br />
+            <a
+              href="https://zapier.com/developer/public-invite/146751/5889feee916f178bf8bd80fd3d7d19f9/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://zapier.com/developer/public-invite/146751/5889feee916f178bf8bd80fd3d7d19f9/
+              <Icon name="external" className={styles.linkIcon} />
+            </a>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
