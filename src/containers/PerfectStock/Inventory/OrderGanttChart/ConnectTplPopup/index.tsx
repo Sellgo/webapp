eae548cb1bc5d650fdab5363edd3db0b697e3542
pongzers <you@example.com>
2022-03-21 13:05:56 -0700

    remove logs

--- src/containers/PerfectStock/Inventory/OrderGanttChart/ConnectTplPopup/index.tsx
+++ src/containers/PerfectStock/Inventory/OrderGanttChart/ConnectTplPopup/index.tsx
@@ -44,7 +44,6 @@ const ConnectTplPopup = (props: Props) => {
   const [selectedTplVendor, setSelectedTplVendor] = React.useState<number | null>(
     connectTplDetails.selectedVendor
   );
-  console.log(tplVendors);
   React.useEffect(() => {
     if (tplVendors.length === 0) {
       fetchTplVendors();
