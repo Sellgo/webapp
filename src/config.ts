dd4aa8825d71b436bda5689e79e6f2b0e3da401e
pongzers <you@example.com>
2022-04-15 08:59:48 -0700

    Update google analytics

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ const PROD_AUTH_CONFIG = {
 
 const DEV_ANALYTICS = {
   // gaTrackingId: 'UA-154653584-1',
-  gaTrackingId: 'G-1SKP5HZZJT',
+  gaTrackingId: 'G-DXLY1B7JRQ',
   fullStoryOrgId: 'Q36Y3',
 };
 
