c1a4a80bc42d5f39b5b7d619389b07ad040643db
pongzers <you@example.com>
2022-05-17 01:48:01 -0700

    update consatnts for aistock

--- src/containers/NewAistockSubscription/AistockCheckOutForm/index.tsx
+++ src/containers/NewAistockSubscription/AistockCheckOutForm/index.tsx
@@ -17,7 +17,7 @@ import StepsInfo from '../../../components/StepsInfo';
 
 /* Constants */
 import { Length, Name, validateEmail } from '../../../constants/Validators';
-import { getSubscriptionID } from '../../../constants/Subscription/Sellgo';
+import { getSubscriptionID } from '../../../constants/Subscription/AiStock';
 
 /* App Config */
 import { AppConfig } from '../../../config';
