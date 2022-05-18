c7daa1a88027f9c05a0cea50afceac07a6724a87
Richard <richard@sellgo.com>
2022-05-18 15:17:25 -0700

    RP ww20.2

--- src/containers/UpsellCtaPage/Aistock/index.tsx
+++ src/containers/UpsellCtaPage/Aistock/index.tsx
@@ -16,27 +16,36 @@ const UpsellCtaPage = () => {
   return (
     <main className={styles.upsellCtaPage}>
       <div className={styles.leftSection}>
-        <h2>The trial session has expired.</h2>
-        <p>But don&apos;t worry ...</p>
+        <h2>Sorry, your free trial subscription has expired.</h2>
+        <p>
+          Make more strategic business decision while always liquid with valuable forecast from our
+          inventory and cash flow forecast tool.
+        </p>
         <div className={styles.benefitsTable}>
           <div className={styles.benefitsColumn}>
             <h2>Benefits</h2>
-            <span>Benefit 1</span>
-            <span>Benefit 2</span>
-            <span>Benefit 3</span>
+            <span>Enable more than 5+ SKUs</span>
+            <span>Streamline smart order for longer than 6-month period</span>
+            <span>Unlock advance sales forecast with seasonality and deals</span>
+            <span>Enable smart order volume-bound optimization </span>
+            <span>Unlock cash flow tool</span>
           </div>
           <div className={styles.freePlanColumn}>
             <h2>Free</h2>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
+            <span>-</span>
+            <span>-</span>
+            <span>-</span>
+            <span>-</span>
+            <span>-</span>
           </div>
           <div className={styles.professionalColumn}>
             <h2>Professional</h2>
             <p className={styles.price}>$97/mo</p>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
+            <span>Yes</span>
+            <span>Yes</span>
+            <span>Yes</span>
+            <span>Yes</span>
+            <span>Yes</span>
           </div>
         </div>
         <ActionButton
@@ -46,7 +55,7 @@ const UpsellCtaPage = () => {
           onClick={redirectToPricing}
           className={styles.activateButton}
         >
-          Activate Now
+          Activate now
         </ActionButton>
       </div>
       <img src={UpsellPicture} alt="upsell image" className={styles.upsellImg} />
