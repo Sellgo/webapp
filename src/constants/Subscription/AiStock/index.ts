85c341b445268e59f0ae168552debc5d42d51305
pongzers <you@example.com>
2022-06-15 17:02:17 -0700

    Update to 104

--- src/constants/Subscription/AiStock/index.ts
+++ src/constants/Subscription/AiStock/index.ts
@@ -6,8 +6,8 @@ export const FREE_TRIAL_SUBSCRIPTION_ID = 100;
 /* Used in webapp pricing page */
 export const MONTHLY_AND_ANNUAL_PLANS = [
   {
-    id: 101, // subscriptionID if needed
-    name: 'Starter',
+    id: 104, // subscriptionID if needed
+    name: 'AiStock',
     productsDatabase: 0,
     salesEstimateCount: 1000,
     monthlyPrice: 97,
@@ -26,102 +26,25 @@ export const MONTHLY_AND_ANNUAL_PLANS = [
       },
     ],
   },
-  {
-    id: 102,
-    name: 'Professional',
-    productsDatabase: 0,
-    salesEstimateCount: 2000,
-    monthlyPrice: 197,
-    annualPrice: 1884,
-    featureSubName: 'Full single-user access, plus',
-    desc: `Ideal for sellers with revenue up to USD $300K/mo`,
-    isNew: true,
-    featuresLists: [
-      {
-        title: 'Ai-based Smart Order',
-        featuresIncluded: [
-          'Smart order advanced optimization',
-          'Smart order future 24 month',
-          '50 SKUs per Amazon store',
-          '13,000 units sold/ mo',
-        ],
-      },
-    ],
-  },
-  {
-    id: 103,
-    name: 'Team',
-    productsDatabase: 0,
-    salesEstimateCount: 3000,
-    monthlyPrice: 297,
-    annualPrice: 2844,
-    featureSubName: 'Everything in professional plan, plus',
-    desc: `Ideal for sellers with revenue up to USD $500K/mo`,
-    featuresLists: [
-      {
-        title: 'Product Research',
-        featuresIncluded: [
-          'Full access to Chrome Extension Sales Estimation',
-          'Full access to Product Research (incoming soon)',
-          'Historical Data up to 1-year',
-        ],
-      },
-      {
-        title: 'Seller Research',
-        featuresIncluded: [
-          'Full use of Seller Database/ Map',
-          'Seller Map Top View 20,000 Sellers*',
-        ],
-      },
-      {
-        title: 'Keyword Research',
-        featuresIncluded: [
-          'Full Access to Keyword Finder/ Database',
-          'True Rank Performance index',
-          'PPC Recampaign with Zapier integration*',
-        ],
-      },
-      {
-        title: 'Advanced access',
-        featuresIncluded: ['Multi-user login', 'Priority onboarding'],
-      },
-    ],
-  },
 ];
 
 /* Used in Change plan Modal */
 export const SUBSCRIPTION_PLANS: SubscriptionPlan[] = [
   {
-    subscriptionId: 101, // subscriptionID if needed
-    name: 'Aistock Starter',
+    subscriptionId: 104, // subscriptionID if needed
+    name: 'Aistock',
     dailyPrice: 0,
     monthlyPrice: 37,
     annualPrice: 324,
-    isDailyPlan: false,
-  },
-  {
-    subscriptionId: 102, // subscriptionID if needed
-    name: 'Aistock Professional',
-    dailyPrice: 0,
-    monthlyPrice: 97,
-    annualPrice: 924,
-    isDailyPlan: false,
-  },
-  {
-    subscriptionId: 103, // subscriptionID if needed
-    name: 'Aistock Team',
-    dailyPrice: 0,
-    monthlyPrice: 177,
-    annualPrice: 1764,
     isDailyPlan: false,
   },
 ];
 
 /* Used in Summary page */
 export const SUBSCRIPTION_DETAILS: { [key: string]: SummaryDetails } = {
-  starter: {
-    name: 'Aistock Starter',
-    id: 101,
+  aistock: {
+    name: 'Aistock',
+    id: 104,
     idWithLegacyPlans: [],
     monthlyPrice: 97,
     annualPrice: 924,
@@ -134,54 +57,14 @@ export const SUBSCRIPTION_DETAILS: { [key: string]: SummaryDetails } = {
       'Sellgo support',
     ],
   },
-  professional: {
-    name: 'Aistock Professional',
-    id: 102,
-    idWithLegacyPlans: [],
-    monthlyPrice: 197,
-    annualPrice: 1884,
-    dailyPrice: -1,
-    subDescription: '7-Days Money Back Guarantee',
-    benefits: [
-      'Full access in Chrome Extension + Sales Estimation',
-      '1,500 Sellers in Seller Database or Map per month',
-      'Full access (export) of Seller Research (Annual Pay)',
-      '5,000 tracking keywords in the Product Rank Tracker',
-      'Unlimited access in our Profit Finder',
-      'Zapier Integration',
-    ],
-  },
-  team: {
-    name: 'Aistock Team',
-    id: 103,
-    idWithLegacyPlans: [],
-    monthlyPrice: 177,
-    annualPrice: 1764,
-    dailyPrice: -1,
-    subDescription: '7-Days Money Back Guarantee',
-    benefits: [
-      'Full access in Chrome Extension + Sales Estimation',
-      '5,000 Sellers in Seller Database or Map per month',
-      'Full access (export) of Seller Research (Annual Pay)',
-      '8,000 tracking keywords in the Product Rank Tracker',
-      'Unlimited access in our Profit Finder',
-      'Zapier Integration',
-    ],
-  },
 };
 
 export const PAYMENT_MODES = ['daily', 'monthly', 'yearly'];
 
-export const SUBSCRIPTION_DETAILS_LIST: SummaryDetails[] = [
-  SUBSCRIPTION_DETAILS.starter,
-  SUBSCRIPTION_DETAILS.professional,
-  SUBSCRIPTION_DETAILS.team,
-];
+export const SUBSCRIPTION_DETAILS_LIST: SummaryDetails[] = [SUBSCRIPTION_DETAILS.aistock];
 
 export const subscriptionDetailsMapping: { [key: string]: number } = {
-  starter: 101,
-  professional: 102,
-  team: 103,
+  aistock: 104,
 };
 
 export const getSubscriptionID = (planName: string) => {
