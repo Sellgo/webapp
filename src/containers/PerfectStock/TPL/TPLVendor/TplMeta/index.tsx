a4df48817a2f693374bc708ce7ae74d7c94857c5
MuhammadMuzzamil <muzzamil@shopdev.co>
2022-07-06 04:31:25 +0500

    BUGFR-4012 Fixes export fba shipping quantity button added on 3PL

--- src/containers/PerfectStock/TPL/TPLVendor/TplMeta/index.tsx
+++ src/containers/PerfectStock/TPL/TPLVendor/TplMeta/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Loader } from 'semantic-ui-react';
 
 /* Styling */
 import styles from './index.module.scss';
+import { DAILY_SUBSCRIPTION_PLANS } from '../../../../../constants/Subscription/Sellgo';
 
 /* Components */
 import ActionButton from '../../../../../components/ActionButton';
@@ -16,20 +18,56 @@ import { fetchTplSkuData } from '../../../../../actions/PerfectStock/Tpl';
 
 /* Types */
 import { TplVendor } from '../../../../../interfaces/PerfectStock/Tpl';
+import { SellerSubscription } from '../../../../../interfaces/Seller';
 
+/* Assets */
+import BanIcon from '../../../../../assets/images/banIcon.svg';
 interface Props {
   fetchTplSkuData: () => void;
   tplSkuData: any[];
   activeTplVendor: TplVendor;
+  sellerSubscription: SellerSubscription;
+  label: string;
+  onButtonClick?: () => void;
+  loading: boolean;
 }
 
 const TplMeta = (props: Props) => {
-  const { fetchTplSkuData, tplSkuData, activeTplVendor } = props;
+  const {
+    fetchTplSkuData,
+    tplSkuData,
+    activeTplVendor,
+    sellerSubscription,
+    label,
+    onButtonClick,
+    loading,
+  } = props;
   const [isReconciling, setIsReconciling] = React.useState<boolean>(false);
+
+  const isExportAllowed = !DAILY_SUBSCRIPTION_PLANS.includes(sellerSubscription.subscription_id);
 
   return (
     <>
       <div className={styles.exportsContainer}>
+        <button
+          className={`${styles.exportBtn}`}
+          onClick={e => {
+            e.preventDefault();
+            e.stopPropagation();
+            onButtonClick && onButtonClick();
+          }}
+          disabled={!isExportAllowed}
+        >
+          {isExportAllowed && !loading ? (
+            label
+          ) : isExportAllowed && loading ? (
+            <div className={styles.loaderContainer}>
+              <Loader active size="tiny" />
+            </div>
+          ) : (
+            <img src={BanIcon} alt="ban-icon" className={styles.banIcon} />
+          )}
+        </button>
         <ActionButton
           variant="secondary"
           size={'md'}
@@ -55,6 +93,7 @@ const TplMeta = (props: Props) => {
 const mapStateToProps = (state: any) => ({
   tplSkuData: getTplSkuData(state),
   activeTplVendor: getTplActiveVendor(state),
+  sellerSubscription: state.subscription.sellerSubscription,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
