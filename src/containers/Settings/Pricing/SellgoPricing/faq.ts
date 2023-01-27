b84e7102b24b866e5744aae4be55907d52580c55
Richard <richard@sellgo.com>
2023-01-27 14:56:34 -0800

    update in-app pricing page

--- src/containers/Settings/Pricing/SellgoPricing/faq.ts
+++ src/containers/Settings/Pricing/SellgoPricing/faq.ts
@@ -2,38 +2,51 @@
 export const faqs = [
   {
     question: 'Is there a free trial?',
+    answer: 'Of course, just sign up easily with free account and enjoy your free account forever.',
+  },
+  {
+    question: 'What do I get with free account?',
+    answer: 'We include 10 free lookups and 100 free searches per month.',
+  },
+  {
+    question: 'How can I learn more about Enterprise account?',
     answer:
-      "While we don't offer a free trial, we do offer a 7-day month back guarantee on our standard plans. If you decide the product isn't the right for within 7 days we will give you a refund, no questions asked. Simply email us at support@sellgo.com to speak to the team.",
+      'Click "Talk to an expert" above and our sales team will reach you out as soon as humanly possible.',
   },
   {
     question: 'What marketplaces are supported?',
     answer:
-      'Sellgo is currently compatible with many Amazon marketplaces. Such as United States, Canada, Mexico, United Kingdom, France, Germany, Italy, and Spain. Some features on Sellgo are currently limited to the U.S. market.',
+      'Sellgo is currently compatible with many Amazon marketplaces. Such as United States, Canada, Mexico, United Kingdom, France, Germany, Italy, and Spain.',
   },
   {
     question: "What is Sellgo's product?",
     answer:
-      'Sellgo is a web App where you can find features for bulk analysis, seller research, product research, etc. Extension is our browser extension for Chrome that provides more product data when you are browsing Amazon search pages.',
-  },
-  {
-    question: 'How many users can I have on my account?',
-    answer:
-      'Sellgo works great for any kind of teams. If you need more access, we encourage you to add users to your account, each with their own secured login access. Our Basic plan is limited to 1 user. Our Professional plan includes 1 user, with the option to add more users you need at $39/month per access (or $407/yr on an annual plan). Our Team plan includes 7 users, but you can add more users at $39/month per access (or $407/yr on an annual plan).',
-  },
-  {
-    question: 'What information can be given to authorized users?',
-    answer:
-      "As the account owner, you have complete control of your account. You can customize access for additional users through Login Management settings. The additional users will not have access to billing information, your credit card number, nor have authorization to make changes to your account. All additional user's access can be added through invite, and revoked anytime.",
+      'Sellgo is the #1 Amazon seller database that is supporting the whole e-commerce ecosystem. You can access and download leads through web app where you can find advance information, i.e. company performance, decision maker information etc.',
   },
   {
     question: 'Can I cancel anytime?',
     answer:
-      'Yes, there are no contractual obligations further than your current billing cycle. If you cancel, your subscription will stop following the current term with no further charges. You can also upgrade your plan at any time and any incremental charges will be prorated. If you are a new customer, all priced plans offer a 7-day money-back guarantee (special promotions or trials excluded). If you cancel within this 7-day period, you will receive a full refund.',
+      'Yes, there are no contractual obligations further than your current billing cycle. You can cancel from "my billing" section and your subscription will stop following the current term with no further charges. You can also upgrade or downgrade your plan at any time and any incremental charges will be prorated.',
+  },
+  {
+    question: 'What is your refund policy?',
+    answer:
+      "we do offer a 7-day money-back guarantee on our standard plans for every new customers (special promotions or trials excluded). If you decide the product isn't the right for within 7 days we will give you a full refund, no questions asked. Simply email us at support@sellgo.com to speak to the team if you have additional questions.",
   },
   {
     question: 'What forms of payment do you accept?',
     answer:
       'Sellgo accepts secure onsite payment processing and accepts Visa, Mastercard, and American Express. If you do not have a credit card, email us at support@sellgo.com and we can send you a Paypal invoice.',
+  },
+  {
+    question: 'How many users can I have on my account?',
+    answer:
+      'Our Starter/ Professional/ Elite plan is limited to 1 user. Our Enterprise plan works great for any kind of teams.',
+  },
+  {
+    question: 'What information can be given to authorized users?',
+    answer:
+      "As the account owner, you have complete control of your account. You can customize access for additional users through Login Management settings. The additional users will not have access to billing information, your credit card number, nor have authorization to make changes to your account. All additional user's access can be added through invite, and revoked anytime.",
   },
   {
     question: 'Will I pay the same price every month?',
