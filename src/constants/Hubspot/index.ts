1735660e8306634001ae263d88a9e88534c71a1e
M-Muzzamil <muhammad@sellgo.com>
2023-03-22 01:44:01 +0500

    Add type check, beta and disable default fields in hubspot

--- src/constants/Hubspot/index.ts
+++ src/constants/Hubspot/index.ts
@@ -7,7 +7,11 @@ export const canSubmitHubspotMappings = (
   let errMsg = '';
   for (let index = 0; index < hubspotMappings.length; index++) {
     const { sellgo_prop_type, hubspot_prop, sellgo_prop } = hubspotMappings[index];
-    if (sellgo_prop_type !== hubspotPropertyType[hubspot_prop]) {
+    if (
+      hubspot_prop &&
+      hubspotPropertyType[hubspot_prop] &&
+      sellgo_prop_type !== hubspotPropertyType[hubspot_prop]
+    ) {
       errMsg = `${hubspot_prop} property cannot be mapped with ${sellgo_prop} because of type difference`;
       break;
     }
