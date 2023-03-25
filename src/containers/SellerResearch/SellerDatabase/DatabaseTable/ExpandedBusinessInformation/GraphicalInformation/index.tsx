398483df85d44dc95dcd4b51b54305494ca61fc3
M-Muzzamil <muhammad@sellgo.com>
2023-03-25 05:08:20 +0500

    graph latest

--- src/containers/SellerResearch/SellerDatabase/DatabaseTable/ExpandedBusinessInformation/GraphicalInformation/index.tsx
+++ src/containers/SellerResearch/SellerDatabase/DatabaseTable/ExpandedBusinessInformation/GraphicalInformation/index.tsx
@@ -100,41 +100,65 @@ const GraphicalInformation = (props: Props) => {
   return (
     <section className={className}>
       {/* Businnes informatiob */}
-      <div className={styles.graphBlock}>
-        <div>
-          <p>Total brands {rowData?.whls_total_brand ?? 0}</p>
-          <div
-            id={`pie-graph-brands-${rowData.merchant_id}`}
-            className={`${styles.pieChart} ${styles.pieChart__brands}`}
-          />
-        </div>
-
-        <div>
-          <p>Total Product {rowData?.whls_total_product ?? 0}</p>
-          <div
-            id={`pie-graph-products-${rowData.merchant_id}`}
-            className={`${styles.pieChart} ${styles.pieChart__products}`}
-          />
-        </div>
-      </div>
-      <div className={styles.graphBlock}>
-        <div>
+      <div className={styles.graphs}>
+        <div className={styles.graphBlock}>
           <div>
-            <p>Brand ownership</p>
-
+            <p>Total brands {rowData?.whls_total_brand ?? 0}</p>
             <div
-              id={`pie-graph-brand-ownership-${rowData.merchant_id}`}
-              className={`${styles.pieChart} ${styles.pieChart__brandsOwnership}`}
+              id={`pie-graph-brands-${rowData.merchant_id}`}
+              className={`${styles.pieChart} ${styles.pieChart__brands}`}
+            />
+          </div>
+
+          <div>
+            <p>Total Product {rowData?.whls_total_product ?? 0}</p>
+            <div
+              id={`pie-graph-products-${rowData.merchant_id}`}
+              className={`${styles.pieChart} ${styles.pieChart__products}`}
             />
           </div>
         </div>
+        <div className={styles.graphBlock}>
+          <div>
+            <div>
+              <p>Brand ownership</p>
 
-        <div>
-          <p>FBA percentage</p>
-          <div
-            id={`pie-graph-fba-percentage-${rowData.merchant_id}`}
-            className={`${styles.pieChart} ${styles.pieChart__fba}`}
-          />
+              <div
+                id={`pie-graph-brand-ownership-${rowData.merchant_id}`}
+                className={`${styles.pieChart} ${styles.pieChart__brandsOwnership}`}
+              />
+            </div>
+          </div>
+
+          <div>
+            <p>FBA percentage</p>
+            <div
+              id={`pie-graph-fba-percentage-${rowData.merchant_id}`}
+              className={`${styles.pieChart} ${styles.pieChart__fba}`}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.informationBlocks}>
+        <div className={styles.informationBlock}>
+          <div className={styles.informationBlock__details}>
+            <p className={styles.informationBlock__details_title}>Primary Category</p>
+            <p className={styles.informationBlock__details_description}>Category</p>
+          </div>
+          <div className={styles.informationBlock__details}>
+            <p className={styles.informationBlock__details_title}>Sub category</p>
+            <p className={styles.informationBlock__details_description}>Sub category</p>
+          </div>
+        </div>
+        <div className={styles.informationBlock}>
+          <div className={styles.informationBlock__details}>
+            <p className={styles.informationBlock__details_title}>Single brand?</p>
+            <p className={styles.informationBlock__details_description}>Yes</p>
+          </div>
+          <div>
+            <p className={styles.informationBlock__details_title}>Close brand</p>
+            <p className={styles.informationBlock__details_description}>Brand name</p>
+          </div>
         </div>
       </div>
     </section>
